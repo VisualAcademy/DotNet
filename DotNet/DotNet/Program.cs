b6fd618c067ec337ca1fae4f3b6e5ba2dc94c1c5
WebCampKR <webcamp@outlook.kr>
2019-01-15 16:44:32 +0900

    GitHub URL 추가

--- DotNet/DotNet/Program.cs
+++ DotNet/DotNet/Program.cs
@@ -7,6 +7,7 @@ namespace DotNet
         static void Main(string[] args)
         {
             Console.WriteLine("C# 책 및 강의에서 사용되는 C# 데모 코드 모음 프로젝트");
+            Console.WriteLine("explorer https://github.com/VisualAcademy/DotNet");
         }
     }
 }
