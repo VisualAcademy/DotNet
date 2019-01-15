90bdd2727662bd8057163b0ae3d1714cef9b00c3
WebCampKR <webcamp@outlook.kr>
2019-01-15 16:41:49 +0900

    DotNet C# Console Project Add

---
+++ DotNet/DotNet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# 책 및 강의에서 사용되는 C# 데모 코드 모음 프로젝트");
+        }
+    }
+}
