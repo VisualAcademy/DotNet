2e93fa38c9a2ecc444cc91d78f6c7e795abac651
RedPlus <redplus@redplus.net>
2019-02-06 06:56:30 +0900

    닷넷 스탠다드 프로젝트와 단위 테스트 프로젝트 추가

---
+++ DotNet/DotNet.Models.Tests/OneTwoTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNet.Models.Tests
+{
+    [TestClass]
+    public class OneTwoTest
+    {
+        [TestMethod]
+        public void OneTwoRepository_GetByMonth_Method_Test()
+        {
+            var repository = new OneTwoRepository();
+
+            var expected = 101.03;
+            var actual = repository.GetByMonth(3).Two;
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DotNet/DotNet.Models/OneTwo.cs
@@ -0,0 +1,33 @@
+﻿namespace DotNet.Models
+{
+    public class OneTwo
+    {
+        public double One { get; set; }
+        public double Two { get; set; }
+    }
+
+    public class OneTwoRepository
+    {
+        public OneTwo GetByMonth(int month)
+        {
+            var r = new OneTwo();
+
+            switch (month)
+            {
+                case 1:
+                    r = new OneTwo { One = 91.8, Two = 110.12 };
+                    break;
+                case 2:
+                    r = new OneTwo { One = 90.3, Two = 110.56 };
+                    break;
+                case 3:
+                    r = new OneTwo { One = 101.15, Two = 101.03 };
+                    break;
+                default:
+                    break;
+            }
+
+            return r;
+        }
+    }
+}
