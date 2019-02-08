c3915a647aab6b862719bd098e813eded7a7ba87
RedPlus <redplus@redplus.net>
2019-02-09 05:17:40 +0900

    ListOfChildToListOfParent

---
+++ DotNet/DotNet/60_IO/ListOfChildToListOfParent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ListOfChildToListOfParent
+{
+    interface A { }
+
+    class B : A { }
+    
+    class ListOfChildToListOfParent
+    {
+        static void Main()
+        {
+            List<A> convertAll = (new List<B>()).ConvertAll(x => (A)x); // [1]
+            List<A> castOf = (new List<B>()).Cast<A>().ToList(); // [2]
+
+            Console.WriteLine(convertAll);
+            Console.WriteLine(castOf);
+        }
+    }
+}
