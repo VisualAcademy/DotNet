c5c309d313b8d5106b197cbdbb48ad46d26b5f28
RedPlus <redplus@redplus.net>
2019-02-18 11:47:17 +0900

    DictionaryOfString.cs

---
+++ DotNet/DotNet/DictionaryOfString.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotNet
+{
+    public class DictionaryOfString
+    {
+        static void Main()
+        {
+            // [1] Dictionary<T> 클래스: 키(Key)와 값(Value)의 쌍으로 컬렉션을 관리
+            Dictionary<string, string> nickNames = new Dictionary<string, string>();
+            nickNames.Add("Taeyo", "태오");
+            nickNames.Add("RedPlus", "레드플러스");
+            nickNames.Add("Itist", "아이티스트");
+
+            // [2] ContainsKey() 메서드로 키 확인
+            if (nickNames.ContainsKey("RedPlus"))
+            {
+                Console.WriteLine(nickNames["RedPlus"]);
+            }
+        }
+    }
+}
