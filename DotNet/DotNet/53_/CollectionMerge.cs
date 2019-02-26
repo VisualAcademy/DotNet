dad69c62ba37c990ac741037e67994aa0c636bc5
RedPlus <redplus@redplus.net>
2019-02-26 23:15:15 +0900

    DictionaryGenericClassDemo

---
+++ DotNet/DotNet/53_/CollectionMerge.cs
@@ -0,0 +1,64 @@
+﻿// [?] 수작업으로 First, Second 컬렉션을 Merge 컬렉션에 포함하기
+using System;
+using System.Collections.Generic;
+
+namespace CollectionMerge
+{
+    class First
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+    }
+
+    class Second
+    {
+        public string B { get; set; }
+        public string C { get; set; }
+    }
+
+    class Merge
+    {
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+    }
+
+    class CollectionMerge
+    {
+        static void Main()
+        {
+            var first = new List<First>()
+            {
+                new First() { A = "F1A", B = "F1B" },
+                new First() { A = "F2A", B = "F2B" }
+            };
+            var second = new List<Second>()
+            {
+                new Second() { B = "S1B", C = "S1C" },
+                new Second() { B = "S2B", C = "S2C" }
+            };
+
+            // Merge 컬렉션 생성 
+            var merge = new List<Merge>();
+
+            // first 컬렉션 추가: for 문 사용
+            for (int i = 0; i < first.Count; i++)
+            {
+                merge.Add(new Merge() { A = first[i].A, B = first[i].B });
+            }
+
+            // second 컬렉션 추가: foreach 문 사용
+            foreach (var s in second)
+            {
+                merge.Add(new Merge() { B = s.B, C = s.C });
+            }
+
+            // 합쳐진 컬렉션 출력
+            Console.WriteLine($"{"A ",5} {"B ",5} {"C ",5}");
+            foreach (var m in merge)
+            {
+                Console.WriteLine($"{m.A,5} {m.B,5} {m.C,5}");
+            }
+        }
+    }
+}
---
+++ DotNet/DotNet/53_/DictionaryGenericClassDemo.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+class DictionaryGenericClassDemo
+{
+    static void Main(string[] args)
+    {
+        // [1] Dictionary 클래스의 인스턴스 생성: IDictionary 인터페이스로 받기
+        IDictionary<string, string> data = new Dictionary<string, string>();
+
+        // [2] 데이터 입력
+        data.Add("cs", "C#");
+        data.Add("aspx", "ASP.NET");
+
+        // [3] 데이터 삭제
+        data.Remove("aspx");
+
+        // [4] 인덱서를 사용해서 데이터 입력
+        data["cshtml"] = "ASP.NET MVC";
+
+        // [5] 키값 중복 불가: 에러 발생
+        try
+        {
+            data.Add("cs", "CSharp");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+
+        // [6] 출력: foreach (var item in data)로 줄여 표현 가능
+        foreach (KeyValuePair<string, string> item in data)
+        {
+            Console.WriteLine("{0}은(는) {1}의 확장자입니다.", item.Key, item.Value);
+        }
+
+        // [7] 인덱서를 사용해서 출력 가능
+        Console.WriteLine(data["cs"]);
+
+        // [8] 없는 키 출력: 에러 발생
+        try
+        {
+            Console.WriteLine(data["vb"]);
+        }
+        catch (KeyNotFoundException knfe)
+        {
+            Console.WriteLine(knfe.Message);
+        }
+
+        // [9] cs 키가 있으면 csharp 변수에 담아서 출력 
+        if (data.TryGetValue("cs", out var csharp))
+        {
+            Console.WriteLine(csharp);
+        }
+        else
+        {
+            Console.WriteLine("cs 키가 없습니다.");
+        }
+
+        // [10] 키값이 없으면 입력하고 출력
+        if (!data.ContainsKey("json"))
+        {
+            data.Add("json", "JSON");
+            Console.WriteLine(data["json"]);
+        }
+
+        // [11] Value 들만 따로 뽑아서 출력
+        var values = data.Values;
+        foreach (string item in values)
+        {
+            Console.WriteLine(item);
+        }
+
+        // [12] Key 들만 따로 뽑아서 출력
+        var keys = data.Keys;
+        foreach (string item in keys)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
