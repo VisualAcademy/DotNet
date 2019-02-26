a4929b1ef5bf6a6616d265ba9d24a53a9a9223c2
RedPlus <redplus@redplus.net>
2019-02-27 00:04:33 +0900

    OutVariableDemo

---
+++ DotNet/DotNet/62_/OutVariable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class OutVariable
+{
+    static void Main()
+    {
+        // [1] C# 6.0까지의 사용 방법: 변수를 미리 선언
+        int r;
+        if (int.TryParse("안녕", out r))
+        {
+            // "안녕"은 int 형으로 변환이 불가능하기에 이 코드는 실행되지 않음
+            Console.WriteLine("{0}", r); 
+        }
+
+        // [2] C# 7.0 이후로의 out var 방식
+        if (int.TryParse("1234", out var result))
+        {
+            // "1234"는 int 형식으로 변환이 가능하기에 result 선언과 동시에 1234가 저장됨
+            Console.WriteLine(result); // 1234
+        }
+        Console.WriteLine(result); // 1234 : if문에서 밖에서도 사용 가능
+    }
+}
---
+++ DotNet/DotNet/62_/OutVariableDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class OutVariableDemo
+{
+    static void Main()
+    {
+        if (DateTime.TryParse("2019/12/25", out var xmas))
+        {
+            Console.WriteLine(xmas);
+        }
+    }
+}
