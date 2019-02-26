ade42c4bb191dea0b29294bbd431dd378be66ba8
RedPlus <redplus@redplus.net>
2019-02-26 23:45:01 +0900

    ExtensionMethodDemo

---
+++ DotNet/DotNet/55_ExtensionMethod/ExtensionMethodDemo/ExtensionMethodDemo.cs
@@ -0,0 +1,30 @@
+﻿// 확장 메서드: 기존 형식에 새로운 메서드를 추가하는 방법
+using System;
+
+namespace ExtensionMethodDemo
+{
+    class ExtensionMethodDemo
+    {
+        static void Main()
+        {
+            string s = "안녕하세요? 확장 메서드... ...";
+            Console.WriteLine(s.Length); // [1] 문자의 개수
+            Console.WriteLine(s.WordCount()); // [2] 단어의 개수
+        }
+    }
+
+    // static 정적(고정) == shared(공유)
+    public static class MyClass
+    {
+        /// <summary>
+        /// 특정 문자열의 단어 개수
+        /// </summary>
+        /// <param name="str">문자열</param>
+        /// <returns>단어 개수</returns>
+        public static int WordCount(this String str)
+        {
+            return str.Split(new char[] { ' ', '.', '?' },
+                StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+    }
+}
---
+++ DotNet/DotNet/55_ExtensionMethod/ExtensionMethodDescription/ExtensionMethodDescription.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+/// <summary>
+/// static 클래스
+/// </summary>
+public static class ExtensionMethodClass
+{
+    /// <summary>
+    /// static 메서드
+    /// </summary>
+    public static StringBuilder AppendCheck(
+        this StringBuilder sb,
+        bool isCheck) => isCheck ? sb.Append(" checked ") : sb;
+}
+
+class ExtensionMethodDescription
+{
+    static void Main()
+    {
+        string checkBox = new StringBuilder()
+            .AppendFormat("<input type=\"{0}\"", "checkbox")
+            .AppendCheck(true) // true를 주면 checked 문자열이 포함되어 출력
+            .Append("/>").ToString();
+        Console.WriteLine(checkBox);
+    }
+}
