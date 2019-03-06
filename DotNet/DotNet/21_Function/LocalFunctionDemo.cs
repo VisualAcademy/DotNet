ff62b12882f8779f51113ad3e41b076629d4febb
RedPlus <redplus@redplus.net>
2019-03-07 04:11:40 +0900

    LocalFunctionDemo.cs

---
+++ DotNet/DotNet/21_Function/LocalFunctionDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class LocalFunctionDemo
+{
+    static void Main()
+    {
+        Display("로컬 함수");
+
+        // 로컬 함수: 메서드(함수)내에서만 사용되는 또 다른 함수
+        void Display(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
