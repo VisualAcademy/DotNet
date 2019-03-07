1e84cec257d0bd061f6c1cb52de8c5024d19811c
RedPlus <redplus@redplus.net>
2019-03-08 01:31:11 +0900

    AsyncAwaitSimple.cs

---
+++ DotNet/DotNet/AsyncAwaitWithHttpClient.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class AsyncAwaitWithHttpClient
+{
+    static void Main()
+    {
+        // 
+    }
+
+    static async Task DoAsync()
+    {
+        try
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync("https://www.google.com");
+
+                var content = await response.Content.ReadAsStreamAsync();
+            }
+        }
+        catch (WebException ex) when (ex.Status == WebExceptionStatus.Timeout)
+        {
+            // 다시 요청
+        }
+    }
+}
