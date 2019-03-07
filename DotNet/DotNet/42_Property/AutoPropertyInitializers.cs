dff6017ceb59f1ad9a15117ff3e344f85233dd5f
RedPlus <redplus@redplus.net>
2019-03-08 00:22:45 +0900

    AwaitWithTryCatchFinally.cs

---
+++ DotNet/DotNet/42_Property/AutoPropertyInitializers.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class AutoPropertyInitializers
+{
+    public static string Name { get; set; } = "길벗";
+    static void Main()
+    {
+        Console.WriteLine(Name);
+    }
+}
---
+++ DotNet/DotNet/AwaitWithTryCatchFinally.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class AwaitWithTryCatchFinally
+{
+    static async Task Main()
+    {
+        await DoAsync();
+    }
+
+    static async Task DoAsync()
+    {
+        try
+        {
+            await Task.Delay(1);
+        }
+        catch (Exception)
+        {
+            await Task.Delay(1);
+        }
+        finally
+        {
+            await Task.Delay(1);
+        }
+    }
+}
