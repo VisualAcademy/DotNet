5878a041242af777b3f0d8229c74002f3e746568
RedPlus <redplus@redplus.net>
2019-03-08 02:01:51 +0900

    AsyncMain.cs

---
+++ DotNet/DotNet/AsyncMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class AsyncMain
+{
+    static async Task Main()
+    {
+        await Task.Delay(1000);
+
+        Console.WriteLine("비동기 메인 메서드");
+    }
+}
