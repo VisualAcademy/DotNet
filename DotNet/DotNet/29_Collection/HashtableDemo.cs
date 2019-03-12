c206898de02a4c8c7e79fb7d6395041462c14d82
RedPlus <redplus@redplus.net>
2019-03-12 22:04:37 +0900

    HashtableDemo.cs

---
+++ DotNet/DotNet/29_Collection/HashtableDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+class HashtableDemo
+{
+    static void Main()
+    {
+        //[1] Hashtable의 인스턴스 생성
+        Hashtable hash = new Hashtable();
+
+        //[2] 배열형 인덱서를 사용 가능한 구조 및 문자열 인덱스 사용 가능
+        hash[0] = "닷넷코리아"; // [A] 배열과 같은 n번째 형태 사용 가능
+        hash["닉네임"] = "레드플러스"; // [B] 문자열 인덱스 사용 가능
+        hash["사이트"] = "비주얼아카데미";
+
+        //[3] 직접 출력
+        Console.WriteLine(hash[0]);
+        Console.WriteLine(hash["닉네임"]);
+        Console.WriteLine(hash["사이트"]);
+
+        //[4] key와 value 쌍으로 출력 가능
+        foreach (object o in hash.Keys)
+        {
+            Console.WriteLine(hash[o]);
+        }
+    }
+}
