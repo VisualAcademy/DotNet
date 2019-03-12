5b9eb3804ae983e37794560d5464f19950e8a00c
RedPlus <redplus@redplus.net>
2019-03-12 22:23:06 +0900

    EnumerableDemo.cs

---
+++ DotNet/DotNet/30_GenericClass/EnumerableDemo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+class EnumerableDemo
+{
+    static void Main()
+    {
+        // 1부터 5까지
+        var numbers = Enumerable.Range(1, 5);
+        foreach (var n in numbers)
+            Console.Write("{0}\t", n);
+        Console.WriteLine();
+
+        // -1을 5개
+        var sameNumbers = Enumerable.Repeat(-1, 5);
+        foreach (var n in sameNumbers)
+            Console.Write("{0}\t", n);
+        Console.WriteLine();
+    }
+}
