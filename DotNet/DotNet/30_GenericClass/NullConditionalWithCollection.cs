2f2e8cc5e28d793d147f96ff9d1c8983ff396ed2
RedPlus <redplus@redplus.net>
2019-03-12 22:52:39 +0900

    NullConditionalWithCollection.cs

--- DotNet/DotNet/30_GenericClass/NullConditionalWithCollection.cs
+++ DotNet/DotNet/30_GenericClass/NullConditionalWithCollection.cs
@@ -1,16 +1,16 @@
-﻿//[?] ?. 연산자: 컬렉션이 null이면 null, 그렇지 않으면 뒤에 오는 속성 값 반환
-//[?] ?. 연산자는 엘비스의 머리 모양과 비슷하다고 하여 Elvis 연산자라고도 함
+﻿// ?. 연산자: 컬렉션이 null이면 null, 그렇지 않으면 뒤에 오는 속성 값 반환
+// ?. 연산자는 엘비스의 머리 모양과 비슷하다고 하여 Elvis 연산자라고도 함
 using System;
 using System.Collections.Generic;
 
-class Null_조건부_연산자
+class NullConditionalWithCollection
 {
     static void Main()
     {
         List<string> list = null;
         int? numberOfList;
 
-        //[1] 리스트가 null이면 null 반환
+        // [1] 리스트가 null이면 null 반환
         numberOfList = list?.Count; // null
         if (numberOfList == null)
         {
@@ -20,7 +20,7 @@ class Null_조건부_연산자
         list = new List<string>();
         list.Add("안녕하세요."); list.Add("반갑습니다.");
 
-        //[2] 리스트가 null이 아니므로 Count 속성의 값인 2 반환
+        // [2] 리스트가 null이 아니므로 Count 속성의 값인 2 반환
         numberOfList = list?.Count; // 2
         if (numberOfList != null)
         {
@@ -28,4 +28,3 @@ class Null_조건부_연산자
         }
     }
 }
-
