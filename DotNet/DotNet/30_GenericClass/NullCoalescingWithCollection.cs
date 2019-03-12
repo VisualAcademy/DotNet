047d5cf3d3a8f089315cd45a5060330c052fff65
RedPlus <redplus@redplus.net>
2019-03-12 22:49:03 +0900

    NullCoalescingWithCollection.cs

---
+++ DotNet/DotNet/30_GenericClass/NullCoalescingWithCollection.cs
@@ -0,0 +1,22 @@
+﻿// ?? 연산자: 컬렉션이 null이 아니면 해당 값 반환, null이면 뒤에 지정한 값 반환
+using System;
+using System.Collections.Generic;
+
+class NullCoalescingWithCollection
+{
+    static void Main()
+    {
+        int num;
+        List<string> list;
+
+        // [1] 컬렉션 리스트가 null이면 Count를 읽을 수 없기에 0으로 초기화
+        list = null; 
+        num = list?.Count ?? 0; // null이면 0 반환, 오른쪽 값 사용
+        Console.WriteLine($"[1] 컬렉션 카운트: {num}"); // 0
+
+        // [2] 컬렉션 리스트가 null이 아니면 Count 속성의 값을 사용 
+        list = new List<string>(); list.Add("또 만나요.");
+        num = list?.Count ?? 0; // null이 아니기 때문에 왼쪽 값 사용
+        Console.WriteLine($"[2] 컬렉션 카운트: {num}"); // 1
+    }
+}
---
+++ DotNet/DotNet/30_GenericClass/Null_조건부_연산자.cs
@@ -0,0 +1,31 @@
+﻿//[?] ?. 연산자: 컬렉션이 null이면 null, 그렇지 않으면 뒤에 오는 속성 값 반환
+//[?] ?. 연산자는 엘비스의 머리 모양과 비슷하다고 하여 Elvis 연산자라고도 함
+using System;
+using System.Collections.Generic;
+
+class Null_조건부_연산자
+{
+    static void Main()
+    {
+        List<string> list = null;
+        int? numberOfList;
+
+        //[1] 리스트가 null이면 null 반환
+        numberOfList = list?.Count; // null
+        if (numberOfList == null)
+        {
+            Console.WriteLine($"[1] 리스트 카운트: null");
+        }
+
+        list = new List<string>();
+        list.Add("안녕하세요."); list.Add("반갑습니다.");
+
+        //[2] 리스트가 null이 아니므로 Count 속성의 값인 2 반환
+        numberOfList = list?.Count; // 2
+        if (numberOfList != null)
+        {
+            Console.WriteLine($"[2] 리스트 카운트: {numberOfList}");
+        }
+    }
+}
+
