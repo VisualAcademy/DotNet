fc6a0407477ce95aab957114c735dec95ce630b2
RedPlus <redplus@redplus.net>
2019-03-20 07:46:09 +0900

    코드 정리

---
+++ DotNet/DotNet/40_Property/ArrowDemo/ArrowDemo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Counter
+{
+    // 필드
+    private int count;
+
+    // 속성
+    public int Count
+    {
+        get => count;
+        set => count = value;
+    }
+
+    // 메서드 
+    public void IncreaseCount() => Count++;
+}
+
+class ArrowDemo
+{
+    static Counter counter;
+    static void Main()
+    {
+        counter = new Counter();
+        counter.IncreaseCount();
+        Console.WriteLine($"카운트: {counter.Count}");
+    }
+}
