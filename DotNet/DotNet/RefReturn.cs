27a875fa9c8b3418df8637448d467f02b5c1ae1b
RedPlus <redplus@redplus.net>
2019-03-24 08:05:36 +0900

    RefReturn.cs

---
+++ DotNet/DotNet/RefReturn.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DotNet
+{
+    public class RefReturn
+    {
+        static void Main()
+        {
+            int[] numbers = { 1, 2, 3 };
+
+            Console.WriteLine(numbers[1]); // 2
+
+            RefFunc(numbers, 1) = 1234; // numbers[1] = 1234
+
+            Console.WriteLine(numbers[1]); // 1234
+
+            ref int RefFunc(int[] nums, int position)
+            {
+                return ref nums[position];
+            }
+        }
+    }
+}
