3889af4d528d010985f8f60a269f642206d307b4
RedPlus <redplus@redplus.net>
2019-03-23 12:12:10 +0900

    GetterSetter.cs

---
+++ DotNet/DotNet/40_Property/GetterSetter/GetterSetter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class GetterSetter
+{
+    public string Name { get; set; }
+    public DateTime BirthDate { get; set; }
+    public int Age
+    {
+        get => (DateTime.Now - BirthDate).Days / 365 + 1; 
+    }
+
+    static void Main()
+    {
+        GetterSetter user = new GetterSetter();
+        user.Name = "마이크로소프트";
+        user.BirthDate = new DateTime(1975, 01, 01);
+
+        Console.WriteLine($"{user.Name} 나이 - {user.Age}");
+    }
+}
---
+++ DotNet/DotNet/51_OOP/POCO/POCO.cs
@@ -0,0 +1,12 @@
+﻿namespace DotNet.POCO
+{
+    /// <summary>
+    /// POCO(Plain Old CLR Object) 클래스: 간단한 속성으로만 이루어진 클래스
+    /// </summary>
+    public class PetFish
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Kind { get; set; }
+    }
+}
