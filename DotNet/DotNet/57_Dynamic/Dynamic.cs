b878f6688e52570d310095a05fdffec194e33abc
RedPlus <redplus@redplus.net>
2019-03-24 02:52:44 +0900

    57_Dynamic

---
+++ DotNet/DotNet/57_Dynamic/Dynamic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Dynamic
+{
+    static void Main()
+    {
+        dynamic x;
+
+        x = 1234; // [1] int
+        Console.WriteLine(x); // 1234
+        Console.WriteLine(x.GetType()); // System.Int32
+
+        x = "안녕하세요."; // [2] string
+        Console.WriteLine(x); // "안녕하세요."
+        Console.WriteLine(x.GetType()); // System.String
+    }
+}
---
+++ DotNet/DotNet/57_Dynamic/DynamicallyTypedObjects.cs
@@ -0,0 +1,14 @@
+﻿// dynamic 키워드로 선언된 변수는 런타임 시점에 데이터 형식이 결정된다. 
+using System; 
+
+class DynamicallyTypedObjects
+{
+    static void Main()
+    {
+        dynamic x = 1;          // 컴파일 시 dynamic, 런타임 시 System.Int32
+        dynamic y = "Hello";    // 컴파일 시점에는 dynamic, 런타임 시점에는 System.String 
+
+        Console.WriteLine($"x: {x}, {x.GetType()}"); 
+        Console.WriteLine($"y: {y}, {y.GetType()}");
+    }
+}
