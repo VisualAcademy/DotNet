0a76b93bf2e2c14e6927dd2c706db845bc27209c
RedPlus <redplus@redplus.net>
2019-04-03 07:26:18 +0900

    ProcessGetCurrentProcess.cs

---
+++ DotNet/DotNet/ProcessGetCurrentProcess.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Diagnostics;
+
+class ProcessGetCurrentProcess
+{
+    static void Main()
+    {
+        Console.WriteLine(Process.GetCurrentProcess().ProcessName);
+    }
+}
