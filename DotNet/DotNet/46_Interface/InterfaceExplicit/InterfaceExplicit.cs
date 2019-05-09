790810715b12bbc8b345865167095f4c21435de1
RedPlus <redplus@redplus.net>
2019-05-09 10:37:34 +0900

    코드 정리

---
+++ DotNet/DotNet/46_Interface/InterfaceExplicit/InterfaceExplicit.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+interface IDog
+{
+    void Eat();
+}
+
+interface ICat
+{
+    void Eat();
+}
+
+class Pet : IDog, ICat
+{
+    void IDog.Eat() => Console.WriteLine("Dog Eat"); // [1] 명시적으로 IDog 지정
+
+    void ICat.Eat() => Console.WriteLine("Cat Eat"); // [2] 명시적으로 ICat 지정
+}
+
+class InterfaceExplicit
+{
+    static void Main()
+    {
+        Pet pet = new Pet();
+        ((IDog)pet).Eat(); // [A] pet 개체를 IDog 형식으로 형식 변환
+        ((ICat)pet).Eat(); // [B] pet 개체를 ICat 형식으로 형식 변환
+
+        IDog dog = new Pet();
+        dog.Eat();
+        ICat cat = new Pet();
+        cat.Eat();
+    }
+}
---
+++ DotNet/DotNet/46_Interface/InterfaceFriends.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace InterfaceFriends
+{
+    // [1] 인터페이스: 설계 // Run() 이라는 단어 설계(명시)
+    public interface IStandard { void Run(); }
+
+    // [2] 추상 클래스: 설계 + 구현
+    public abstract class KS
+    {
+        public abstract void Back();
+        public void Left() => Console.WriteLine("좌회전");
+    }
+
+    // [3] 분할 클래스
+    public partial class MyCar : KS
+    {
+        public override void Back() => Console.WriteLine("후진");
+    }
+    public partial class MyCar : KS
+    {
+        public void Right() => Console.WriteLine("우회전");
+    }
+
+    // [4] 봉인(최종) 클래스
+    public sealed class Car : MyCar, IStandard
+    {
+        public void Run() => Console.WriteLine("전진");
+    }
+
+    // [5] 아래 코드는 에러(봉인 클래스는 상속 받을 수 없음)
+    // public class SelfCar : Car { }
+
+    class InterfaceFriends
+    {
+        static void Main()
+        {
+            Car cla = new Car();
+            cla.Run(); cla.Left(); cla.Right(); cla.Back();
+        }
+    }
+}
