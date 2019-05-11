ce2e4b9cf1b6e1c6b4e5c1016cf40adcd9db1e41
RedPlus <redplus@redplus.net>
2019-05-12 06:33:49 +0900

    C# Attribute 소스 추가

---
+++ DotNet/DotNet/48_Attribute/ConditionalDemo.cs
@@ -0,0 +1,16 @@
+﻿#define RELEASE
+using System;
+using System.Diagnostics;
+
+public class ConditionalDemo
+{
+    static void Main()
+    {
+        DebugMethod();
+        ReleaseMethod();
+    }
+
+    [Conditional("DEBUG")]
+    static void DebugMethod() => Console.WriteLine("디버그 환경에서만 표시");
+    [Conditional("RELEASE")] static void ReleaseMethod() => Console.WriteLine("릴리스 환경에서만 표시");
+}
---
+++ DotNet/DotNet/48_Attribute/ObsoleteDemo.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class ObsoleteDemo
+{
+    static void Main()
+    {
+
+    }
+}
