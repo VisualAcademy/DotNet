1c5d8f0864d6acda22ac1fb7525575d1c1d83a64
RedPlus <redplus@redplus.net>
2019-05-20 17:11:44 +0900

    DebugDemo

---
+++ DotNet/DotNet/100_Debug/DebugDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class DebugDemo
+{
+    static void Main()
+    {
+        int number1 = 10;
+        int number2 = 20;
+        int number3 = 30;
+
+        Console.WriteLine(number1);
+        Console.WriteLine(number2);
+        Console.WriteLine(number3);
+    }
+}
