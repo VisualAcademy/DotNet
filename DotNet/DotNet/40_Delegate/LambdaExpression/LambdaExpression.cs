6bd8c38f816a320c43279ae03141cca338e4f033
RedPlus <redplus@redplus.net>
2019-05-31 22:05:38 +0900

    LambdaExpression

---
+++ DotNet/DotNet/40_Delegate/LambdaExpression/LambdaExpression.cs
@@ -0,0 +1,15 @@
+﻿// 람다 식(Lambda Expression): C# 3.0에 도입됨
+using System;
+
+class LambdaExpression
+{
+    //[1] 대리자 선언
+    delegate void Lambda();
+    static void Main()
+    {
+        //[2] 대리자 개체에 람다식 정의: goes to 연산자
+        Lambda hi = () => Console.WriteLine("안녕하세요.");
+        //[3] 대리자 개체 호출
+        hi();
+    }
+}
