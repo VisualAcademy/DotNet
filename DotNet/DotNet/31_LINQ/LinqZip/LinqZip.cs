d00c3f12d5c90b826d80b3d60dcc954bdfc1b99a
RedPlus <redplus@redplus.net>
2019-06-03 15:55:51 +0900

    코드 정리

--- DotNet/DotNet/31_LINQ/LinqZip/LinqZip.cs
+++ DotNet/DotNet/31_LINQ/LinqZip/LinqZip.cs
@@ -1,20 +1,20 @@
 ﻿// LINQ Zip() 확장 메서드: 관련있는 2개의 시퀀스(컬렉션)를 묶어서 출력
+using System;
 using System.Linq;
 
-class LINQ_Zip
+class LinqZip
 {
     static void Main()
     {
         int[] numbers = { 1, 2, 3 }; 
         string[] words = { "하나", "둘" }; 
 
-        var numbersAndWords =
-            numbers.Zip(words, 
-                (first, second) => first + "-" + second);
+        var numbersAndWords = 
+            numbers.Zip(words, (first, second) => first + "-" + second);
 
         foreach (var item in numbersAndWords)
         {
-            System.Console.WriteLine(item); // 1-하나, 2-둘
+            Console.WriteLine(item); // 1-하나, 2-둘
         }
     }
 }
