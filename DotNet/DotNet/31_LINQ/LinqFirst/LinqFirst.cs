70ea70490c899bcee048a16b38de5633e6547f73
RedPlus <redplus@redplus.net>
2019-06-03 14:24:38 +0900

    코드 정리

---
+++ DotNet/DotNet/31_LINQ/LinqFirst/LinqFirst.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LinqFirst
+{
+    static void Main()
+    {
+        List<string> colors = new List<string> { "Red", "Green", "Blue" };
+
+        string red = colors.First(c => c == "Red");
+
+        Console.WriteLine(red); // "Red"
+
+        try
+        {
+            // 없는 데이터 요청시 예외 발생
+            string black = colors.First(color => color == "Black");
+            //// 없는 데이터 요청시 null 값 반환
+            //string black = colors.FirstOrDefault(color => color == "Black");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("예외 발생: " + ex.Message);
+        }
+    }
+}
+//> List<string> colors = new List<string> { "Red", "Green", "Blue" };
+//> colors.First(c => c == "Red")
+//"Red"
+//> colors.First(color => color == "Black")
+//시퀀스에 일치하는 요소가 없습니다.
+//  + System.Linq.Enumerable.First<TSource>(IEnumerable<TSource>, Func<TSource, bool>)
+//> colors.FirstOrDefault(color => color == "Black")
+//null
