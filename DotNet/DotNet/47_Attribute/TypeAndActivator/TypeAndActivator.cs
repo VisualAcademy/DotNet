de132e895908adf87f117ea5c670103be11caaee
RedPlus <redplus@redplus.net>
2019-06-10 08:03:33 +0900

    코드 정리

--- DotNet/DotNet/47_Attribute/TypeAndActivator/TypeAndActivator.cs
+++ DotNet/DotNet/47_Attribute/TypeAndActivator/TypeAndActivator.cs
@@ -2,6 +2,7 @@
 
 namespace TypeAndActivator
 {
+    //[1] 샘플 클래스 및 메서드 생성
     public class MyClass
     {
         public void Test() => 
@@ -12,11 +13,13 @@ namespace TypeAndActivator
     {
         static void Main()
         {
-            // Type.GetType() 메서드에 지정한 클래스 형식을 가져옴
+            //[2] Type.GetType() 메서드에 지정한 클래스 형식을 가져옴
             Type type = Type.GetType("TypeAndActivator.MyClass");
-            // Activatory.CreateInstance() 메서드로 지정된 형식의 인스턴스 생성
+
+            //[3] Activatory.CreateInstance() 메서드로 지정된 형식의 인스턴스 생성
             dynamic objType = Activator.CreateInstance(type);
-            // dynamic 타입의 Test() 메서드를 직접 지정해서 호출 
+
+            //[4] dynamic 타입의 Test() 메서드를 직접 지정해서 호출 
             objType.Test();
         }
     }
---
+++ DotNet/DotNet/49_GenericMake/GenericMethod.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotNet
+{
+    //[1] 제네릭 클래스 설계
+    public class Hello<T>
+    {
+        private T _message; //[A] 필드
+        public Hello() { _message = default(T); } //[B] 기본 생성자
+        public Hello(T message) { this._message = message; } //[C] 매개 변수가 있는 생성자
+        public void Say(T message) => 
+            Console.WriteLine("{0}", message); //[D] 제네릭 메서드
+        public T GetMessage() => this._message; //[E] 일반 메서드
+    }
+
+    class GenericMethod
+    {
+        static void Main()
+        {
+            //[2] 제네릭 클래스 호출
+            Hello<int> hi = new Hello<int>();
+            hi.Say(1234);
+            Hello<string> hs = new Hello<string>();
+            hs.Say("안녕");            
+        }
+    }
+}
