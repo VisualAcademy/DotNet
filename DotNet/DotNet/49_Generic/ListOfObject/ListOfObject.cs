6b9ed9cc5a0ea5c6f919f1ec129b6897d0ced457
RedPlus <redplus@redplus.net>
2019-06-10 01:13:39 +0900

    ListOfObject.cs

--- DotNet/DotNet/49_Generic/ListOfObject/ListOfObject.cs
+++ DotNet/DotNet/49_Generic/ListOfObject/ListOfObject.cs
@@ -1,9 +1,11 @@
 ﻿// List<Object>: 새로운 개체(클래스)의 컬렉션을 담아놓을 수 있는 그릇
 // List<T>: List<int>, List<string>, List<All>
+using System;
 using System.Collections.Generic;
 
 namespace ListOfObject
 {
+    //[1] 지역과 국번 저장 클래스 선언
     public class AreaCode
     {
         public string Number { get; set; }
@@ -13,26 +15,28 @@ namespace ListOfObject
     {
         static void Main()
         {
-            // 제네릭 리스트 생성
+            //[2] 제네릭 리스트 개체 생성
             List<AreaCode> areas = new List<AreaCode>();
 
-            // 컬렉션에 포함될 각각의 개체 생성
-            //[a] 속성으로 개체 초기화
+            //[3] 컬렉션에 포함될 각각의 개체 생성
+            //[3][1] 속성으로 개체 초기화
             AreaCode seoul = new AreaCode();
-            seoul.Number = "02"; seoul.AreaName = "서울";
-            //[b] 개체 이니셜라이저로 개체 초기화
+            seoul.Number = "02";
+            seoul.AreaName = "서울";
+
+            //[3][2] 개체 이니셜라이저로 개체 초기화
             AreaCode sejong = new AreaCode() {
                 Number = "044", AreaName = "세종"
             };            
 
-            // 컬렉션에 개체 등록
-            areas.Add(seoul); areas.Add(sejong);
+            //[4] 컬렉션에 개체 등록
+            areas.Add(seoul);
+            areas.Add(sejong);
 
-            // 컬렉션의 값을 반복해서 속성을 사용해서 출력
+            //[5] 컬렉션의 값을 반복해서 속성을 사용해서 출력
             foreach (var area in areas)
             {
-                System.Console.WriteLine(
-                    $"번호: {area.Number}, 지역: {area.AreaName}");
+                Console.WriteLine($"번호: {area.Number}, 지역: {area.AreaName}");
             }
         }
     }
---
+++ DotNet/DotNet/49_Generic/TypeConstraint.cs
@@ -0,0 +1,21 @@
+﻿public interface IKs { }
+public class GoodCar { }
+public class BadCar { public BadCar(string message) { } }
+public class OfficeCamper : GoodCar, IKs { }
+public class CarValue<T> where T : struct { } // 값 형식만
+public class CarReference<T> where T : class { } // 참조 형식만
+public class CarNew<T> where T : new() { } // Default 생성자
+public class CarClass<T> where T : GoodCar { } // GoodCar에서 파생
+public class CarInterface<T> where T : IKs { } // IKs인터페이스
+
+public class TypeConstraint
+{
+    public static void Main()
+    {
+        CarValue<int> c = new CarValue<int>(); // struct 성공
+        CarReference<string> cs = new CarReference<string>(); // class 성공   
+        CarNew<GoodCar> cn = new CarNew<GoodCar>(); // new() 성공
+        CarClass<OfficeCamper> cc = new CarClass<OfficeCamper>(); // 사용자 정의 타입
+        CarInterface<IKs> h = new CarInterface<IKs>(); // 인터페이스 지정
+    }
+}
