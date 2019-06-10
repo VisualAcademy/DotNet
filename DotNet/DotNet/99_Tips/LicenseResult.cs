872404df78164550bb914b695134b3f5d441f3a4
RedPlus <redplus@redplus.net>
2019-06-11 03:19:13 +0900

    코드 정리

--- DotNet/DotNet/99_Tips/LicenseResult.cs
+++ DotNet/DotNet/99_Tips/LicenseResult.cs
@@ -2,13 +2,7 @@
 
 class LicenseResult
 {
-    static void Main()
-    {
-        Console.WriteLine(GetResultWithStatement(60)); // 합격
-        Console.WriteLine(GetResultWithExpression(60)); // 합격
-    }
-
-    // 문(Statement)을 사용하여 시험 결과 판정
+    //[1] 문(Statement)을 사용하여 시험 결과 판정
     static string GetResultWithStatement(int score)
     {
         string r;
@@ -23,7 +17,13 @@ class LicenseResult
         return r; // 변하는 값 반환
     }
 
-    // 식(Expression)을 사용하여 시험 결과 판정
+    //[2] 식(Expression)을 사용하여 시험 결과 판정
     static string GetResultWithExpression(int score) =>
         score >= 60 ? "합격" : "불합격"; // 변하지 않는 값 반환
+
+    static void Main()
+    {
+        Console.WriteLine(GetResultWithStatement(60)); // 합격
+        Console.WriteLine(GetResultWithExpression(60)); // 합격
+    }
 }
