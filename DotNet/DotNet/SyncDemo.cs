e9d132fca01fdfd3617425d7586557240506135b
RedPlus <redplus@redplus.net>
2019-06-12 12:18:37 +0900

    코드 정리

--- DotNet/DotNet/SyncDemo.cs
+++ DotNet/DotNet/SyncDemo.cs
@@ -2,15 +2,11 @@
 
 class SyncDemo
 {
+    static void Sum(int a, int b) => Console.WriteLine($"{a} + {b} = {a + b}");
     static void Main()
     {
-        Sum(3, 5);
+        Sum(3, 5); // Sum() 메서드는 호출한 순서대로 실행됩니다. 
         Sum(5, 7);
         Sum(7, 9);
-    }
-
-    static void Sum(int a, int b)
-    {
-        Console.WriteLine($"{a} + {b} = {a + b}");
     }
 }
