fd0eb3fafd311adc0d747708f6e6aa1f24ee21b7
RedPlus <redplus@redplus.net>
2019-07-02 23:27:04 +0900

    ForEach() 메서드로 반복 출력하기

---
+++ DotNet/DotNet/31_LINQ/LinqForEach.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LinqForEach
+{
+    static void Main()
+    {
+        var numbers = new List<int>() { 10, 20, 30, 40, 50 };
+
+        numbers.Where(n => n <= 20).ToList().ForEach(n => Console.WriteLine(n));
+    }
+}
