677aafba6c7daaadf5da4d20f88eaa886e3dfd4f
RedPlus <redplus@redplus.net>
2019-07-04 04:12:16 +0900

    if 문 관련 코드 및 파일 정리

--- DotNet/DotNet/15_Else/IfElseIfElse/IfElseIfElse.cs
+++ DotNet/DotNet/15_Else/IfElseIfElse/IfElseIfElse.cs
@@ -7,11 +7,11 @@ class IfElseIfElse
         int number1 = 10;
         int number2 = 20;
 
-        if (number1 > number2)
+        if (number1 > number2) // 조건 처리 1
         {
             Console.WriteLine("number1이 더 큽니다.");
         }
-        else if (number1 < number2)
+        else if (number1 < number2) // 조건 처리 2
         {
             Console.WriteLine("number2가 더 큽니다.");
         }
