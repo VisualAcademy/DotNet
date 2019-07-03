ac25b39fcc59dbbb2bace0a16ffad58e9922a714
RedPlus <redplus@redplus.net>
2019-07-04 02:37:05 +0900

    코드 정리

---
+++ DotNet/DotNet/18_While_Do_ForEach/WhileLoop/WhileLoop.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class WhileLoop
+{
+    static void Main()
+    {
+        int count = 1;                                  // 초기식
+        while (count <= 3)                              // 조건식
+        {
+            Console.WriteLine($"카운트: {count}");       // 실행문
+            count++;                                    // 증감식
+        }
+    }
+}
