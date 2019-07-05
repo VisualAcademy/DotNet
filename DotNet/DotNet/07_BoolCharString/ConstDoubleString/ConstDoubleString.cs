0fc6b4e8ee7fb16c084a18f5b186fa4c9c100b95
RedPlus <redplus@redplus.net>
2019-07-05 22:06:22 +0900

    코드 정리

---
+++ DotNet/DotNet/07_BoolCharString/ConstDoubleString/ConstDoubleString.cs
@@ -0,0 +1,16 @@
+﻿//[!] 상수(Constant): 변하지 않는 변수, 읽기 전용 변수
+using System;
+
+class ConstDoubleString
+{
+    static void Main()
+    {
+        //[1] 상수 선언과 동시에 초기화
+        const double PI = 3.14;
+        const string SITE_NAME = "닷넷코리아";
+
+        //[2] 상수 참조
+        Console.WriteLine(PI); // 3.14
+        Console.WriteLine(SITE_NAME); // 닷넷코리아
+    }
+}
--- DotNet/DotNet/07_BoolCharString/Immutable/Immutable.cs
+++ DotNet/DotNet/07_BoolCharString/Immutable/Immutable.cs
@@ -7,7 +7,7 @@ class Immutable
         // 상수: 변하지 않는 값
         const string name = "박용준";
         // 변수: 변하는 값
-        int age = 42;
+        int age = 45;
 
         Console.WriteLine($"이름: {name}, 나이: {age}");
     }
