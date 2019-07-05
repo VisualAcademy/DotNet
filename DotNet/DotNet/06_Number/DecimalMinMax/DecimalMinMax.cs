11d2b755e1db4f9a8e0db8d4935a9e06f5430147
RedPlus <redplus@redplus.net>
2019-07-05 23:57:40 +0900

    코드 및 파일 정리

---
+++ DotNet/DotNet/06_Number/DecimalMinMax/DecimalMinMax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class DecimalMinMax
+{
+    static void Main()
+    {
+        decimal min = Decimal.MinValue;
+        decimal max = Decimal.MaxValue;
+
+        Console.WriteLine(min); // -79228162514264337593543950335
+        Console.WriteLine(max); // +79228162514264337593543950335
+    }
+}
