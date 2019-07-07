9e2c6c07080229bc4568c90e1dae938a51d61d8b
RedPlus <redplus@redplus.net>
2019-07-07 11:47:40 +0900

    코드 정리

---
+++ DotNet/DotNet/40_Delegate/PredicateDemo/PredicateDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class PredicateDemo
+{
+    static void Main()
+    {
+        Predicate<string> isNullOrEmpty = String.IsNullOrEmpty;
+
+        Console.WriteLine(isNullOrEmpty("Not Null"));
+
+        Predicate<Type> isPrimitive = t => t.IsPrimitive;
+
+        Console.WriteLine(isPrimitive(typeof(int)));
+    }
+}
---
+++ DotNet/DotNet/40_Delegate/StronglyTypedDelegate/StronglyTypedDelegate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class StronglyTypedDelegate
+{
+    // 강력한 형식의 대리자 
+    public delegate double DelegateType(double x, double y);
+
+    static void Main()
+    {
+        DelegateType pow = Math.Pow;
+
+        double actual = pow(2, 10);
+
+        Console.WriteLine(actual);
+    }
+}
