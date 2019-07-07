1478f909729b279638d62df8807eeb1ecd9b70a4
RedPlus <redplus@redplus.net>
2019-07-07 13:45:42 +0900

    무명 메서드와 람다 식 비교

---
+++ DotNet/DotNet/40_Delegate/FuncLambda/FuncLambda.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class FuncLambda
+{
+    static void Main()
+    {
+        //[1] 익명 메서드 담기
+        Func<int, int> anonymous = delegate (int x) { return x * x; };
+        Console.WriteLine(anonymous(2)); // 2 * 2
+
+        //[2] 람다 식 담기
+        Func<int, double> lambda = x => x / (double)2;
+        Console.WriteLine(lambda(3));
+    }
+}
--- DotNet/DotNet/40_Delegate/LambdaExpressionWithFunc/LambdaExpressionWithFunc.cs
+++ DotNet/DotNet/40_Delegate/LambdaExpressionWithFunc/LambdaExpressionWithFunc.cs
@@ -4,8 +4,27 @@ class LambdaExpressionWithFunc
 {
     static void Main()
     {
-        // 매개 변수가 int이고 반환값이 int인 람다 식
+        //[1] 매개 변수가 int이고 반환값이 int인 람다 식
         Func<int, int> square = x => x * x;
         Console.WriteLine(square(3)); // 9 
+
+        //[2] 무명 메서드와 람다 식 비교
+        Func<int> getNumber1 = delegate () { return 1234; };
+        Console.WriteLine(getNumber1());
+
+        Func<int> getNumber2 = () => 1234;
+        Console.WriteLine(getNumber2());
+
+        Func<int, int> addOne1 = delegate (int x) { return x + 1; };
+        Console.WriteLine(addOne1(10));
+
+        Func<int, int> addOne2 = x => x + 1;
+        Console.WriteLine(addOne2(10));
+
+        Func<string, string, string> plus1 = delegate (string a, string b) { return $"{a} {b}"; };
+        Console.WriteLine(plus1("Hello", "World"));
+
+        Func<string, string, string> plus2 = (a, b) => $"{a} {b}";
+        Console.WriteLine(plus2("Hello", "World"));
     }
 }
