e31072c24dd0fff0874361ee3032f237d9f01c48
RedPlus <redplus@redplus.net>
2019-07-09 14:13:16 +0900

    코드 정리

---
+++ DotNet/DotNet/39_Indexer/IteratorFibonacci.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+class IteratorFibonacci
+{
+    static IEnumerable<int> GetFibonacci()
+    {
+        int current = 1;
+        int next = 1;
+
+        yield return current;
+
+        while (true)
+        {
+            int temp = current + next;
+            current = next;
+            next = temp;
+            yield return current; 
+        }
+    }
+    static void Main()
+    {
+        int count = 20;
+        int i = 0;
+        foreach (var f in GetFibonacci())
+        {
+            Console.WriteLine(f);
+            if (++i > count)
+            {
+                break;
+            }
+        }
+    }
+}
