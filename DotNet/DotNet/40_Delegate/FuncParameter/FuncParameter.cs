c6289ca27c9bfbcc854ce49494788672c60bbab5
RedPlus <redplus@redplus.net>
2019-07-09 13:50:08 +0900

    파일 및 코드 정리

---
+++ DotNet/DotNet/40_Delegate/FuncParameter/FuncParameter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class FuncParameter
+{
+    // 메서드의 매개 변수로 람다 식 전달 
+    static void Calc(int x, int y, Func<int, int, int> calc) => Console.WriteLine(calc(x, y));
+    static void Main()
+    {
+        Calc(3, 5, (x, y) => x + y);
+        Calc(3, 5, (x, y) => x * y);
+    }
+}
---
+++ DotNet/DotNet/40_Delegate/LambdaExpressionWithPredicate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LambdaExpressionWithPredicate
+{
+    // 매개 변수로 람다 식을 받는 메서드
+    static IEnumerable<int> FindNumbers(Predicate<int> predicate)
+    {
+        for (int i = 1; i <= 100; i++)
+        {
+            if (predicate(i))
+            {
+                yield return i; 
+            }
+        }
+    }
+
+    static void Main()
+    {
+        var numbers = FindNumbers(f => f % 33 == 0);
+        foreach (var n in numbers)
+        {
+            Console.WriteLine(n);
+        }
+    }
+}
