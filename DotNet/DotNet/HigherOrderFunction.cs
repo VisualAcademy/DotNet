20c92961c9876a473d4ee6e9bc642ca24c7d30f1
RedPlus <redplus@redplus.net>
2019-07-11 15:40:31 +0900

    HigherOrderFunction

---
+++ DotNet/DotNet/HigherOrderFunction.cs
@@ -0,0 +1,44 @@
+﻿// 고차 함수(Higher-Order Function): 함수 자체를 매개 변수 또는 반환값으로 가지는 함수
+using System;
+
+class HigherOrderFunction
+{
+    static void Main()
+    {
+        //[1] Action<T> 매개 변수 전달
+        Action<string> action = msg => Console.WriteLine(msg);
+        FunctionParameterWithAction(action, "고차 함수: 매개 변수");
+
+        //[2] Func<T> 매개 변수 전달
+        Func<int, int> func = x => x * x;
+        FunctionParameterWithFunc(func, 3);
+
+        //[3] Action<T> 반환값 
+        FunctionReturnValueWithAction()("고차 함수: 반환값");
+
+        //[4] Func<T> 반환값
+        int number = 3;
+        int result = FunctionReturnValueWithFunc()(number);
+        Console.WriteLine($"{number} * {number} = {result}"); // 9 
+    }
+
+    //[1] 매개 변수가 Action<T>
+    static void FunctionParameterWithAction(Action<string> action, string message)
+    {
+        action(message);
+    }
+
+    //[2] 매개 변수가 Func<T>
+    static void FunctionParameterWithFunc(Func<int, int> func, int number)
+    {
+        int result = func(number);
+        Console.WriteLine($"{number} * {number} = {result}");
+    }
+
+    //[3] 반환값이 Action<T>
+    static Action<string> FunctionReturnValueWithAction() => 
+        msg => Console.WriteLine($"{msg}");
+
+    //[4] 반환값이 Func<T>
+    static Func<int, int> FunctionReturnValueWithFunc() => x => x * x; 
+}
