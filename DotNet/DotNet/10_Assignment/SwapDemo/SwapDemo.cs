6f90d86b990855c9f20d1a72d9746886f0c433e6
RedPlus <redplus@redplus.net>
2019-07-21 03:03:42 +0900

    변숫값 서로 바꾸기

---
+++ DotNet/DotNet/10_Assignment/SwapDemo/SwapDemo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class SwapDemo
+{
+    static void Main()
+    {
+        var i = 100;
+        var j = 200;
+
+        Console.WriteLine($"처음: {i}, {j}");
+
+        // 변숫값 서로 바꾸기
+        var temp = i; //[A] 임시 변수에 i 값 담기
+        i = j; //[B] i 값에 j 값으로 덮어쓰기
+        j = temp; //[C] j에 i 값이 담겨 있던 temp값으로 덮어쓰기
+
+        Console.WriteLine($"변경: {i}, {j}");
+    }
+}
---
+++ DotNet/DotNet/SingleResponsibilityPrinciple.cs
@@ -0,0 +1,65 @@
+﻿// https://www.youtube.com/watch?v=kvxlJrqc5wM 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace SOLID
+{
+    public class Journal
+    {
+        private readonly List<string> entries = new List<string>();
+
+        private static int count = 0;
+
+        public int AddEntry(string text)
+        {
+            entries.Add($"{++count}: {text}");
+            return count; // memento
+        }
+
+        public void RemoveEntry(int index)
+        {
+            entries.RemoveAt(index);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(Environment.NewLine, entries); 
+        }
+
+        // 영구적으로 저장하는 로직은 따로 빼자
+        //public void Save(string filename)
+        //{
+        //    File.WriteAllText(filename, ToString());
+        //}
+    }
+
+    public class Persistence
+    {
+        public void SaveToFile(Journal j, string filename, bool overwrite = false)
+        {
+            if (overwrite || !File.Exists(filename))
+            {
+                File.WriteAllText(filename, j.ToString());
+            }
+        }
+    }
+    
+    public class SingleResponsibilityPrinciple
+    {
+        static void Main(string[] args)
+        {
+            var j = new Journal();
+            j.AddEntry("ASP.NET");
+            j.AddEntry("Blazor");
+            j.AddEntry("C#");
+            Console.WriteLine(j);
+
+            var p = new Persistence();
+            var filename = @"C:\temp\journal.txt";
+            p.SaveToFile(j, filename, true);
+            Process.Start(filename); 
+        }
+    }
+}
