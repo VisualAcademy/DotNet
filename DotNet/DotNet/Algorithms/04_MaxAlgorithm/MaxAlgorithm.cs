9bb3a5f727b12dee3c68aac75a6171f209828134
RedPlus <redplus@redplus.net>
2019-07-27 11:54:14 +0900

    최솟값 알고리즘(Min Algorithm) 코드 정리

--- DotNet/DotNet/Algorithms/04_MaxAlgorithm/MaxAlgorithm.cs
+++ DotNet/DotNet/Algorithms/04_MaxAlgorithm/MaxAlgorithm.cs
@@ -1,9 +1,35 @@
-﻿using System;
+﻿//[?] 주어진 데이터 중에서 가장 큰 값
+using System;
+using System.Linq;
 
+/// <summary>
+/// 최댓값 알고리즘(Max Algorithm): (주어진 범위 + 주어진 조건)의 자료들의 가장 큰 값
+/// </summary>
 class MaxAlgorithm
 {
     static void Main()
     {
+        //[1] Initialzie
+        int max = int.MinValue; // 정수 형식의 데이터 중 가장 작은 값으로 초기화
 
+        //[2] Input
+        int[] numbers = { -2, -5, -3, -7, -1 };
+
+        //[3] Process: MAX
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > max)
+            {
+                max = numbers[i]; // MAX: 더 큰 값으로 할당 
+            }
+        }
+
+        //[4] Output
+        Console.WriteLine($"최댓값(식): {numbers.Max()}");
+        Console.WriteLine($"최댓값(문): {max}");
     }
 }
+
+//int[] numbers = { -2, -5, -3, -7, -1 };
+//numbers.Max()
+//-1
--- DotNet/DotNet/Algorithms/05_MinAlgorithm/MinAlgorithm.cs
+++ DotNet/DotNet/Algorithms/05_MinAlgorithm/MinAlgorithm.cs
@@ -1,9 +1,36 @@
-﻿using System;
+﻿//[?] 주어진 데이터 중에서 가장 작은 [짝수] 값
+using System;
+using System.Linq;
+using static System.Console;
 
+/// <summary>
+/// 최솟값 알고리즘(Min Algorithm): (주어진 범위 + 주어진 조건)의 자료들의 가장 작은 값
+/// </summary>
 class MinAlgorithm
 {
     static void Main()
     {
+        //[1] Initialzie
+        var min = Int32.MaxValue; // 정수 형식의 데이터 중 가장 큰 값으로 초기화
 
+        //[2] Input: 이진수로 표현 + 숫자 구분자
+        int[] numbers = { 0b0010, 0B_0101, 0b0011, 0B_0111, 0b0000_0001 };
+
+        //[3] Process: MIN
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] < min && numbers[i] % 2 == 0)
+            {
+                min = numbers[i]; // MIN: 더 작은 값으로 할당 
+            }
+        }
+
+        //[4] Output
+        WriteLine($"짝수 최솟값(식): {numbers.Where(n => n % 2 == 0).Min()}");
+        WriteLine($"짝수 최솟값(문): {min}");
     }
 }
+
+//int[] numbers = { 0b0010, 0B_0101, 0b0011, 0B_0111, 0b0000_0001 };
+//numbers.Where(n => n % 2 == 0).Min()
+//2
