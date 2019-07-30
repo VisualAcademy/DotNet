6a3125c35ef3dcf400ebc178563bb07decf93046
RedPlus <redplus@redplus.net>
2019-07-31 02:42:58 +0900

    코드 정리

--- DotNet/DotNet/Algorithms/06_NearAlgorithm/NearAlgorithm.cs
+++ DotNet/DotNet/Algorithms/06_NearAlgorithm/NearAlgorithm.cs
@@ -11,15 +11,15 @@ class NearAlgorithm
     static void Main()
     {
         //[0] 절댓값 구하기 로컬 함수: Math.Abs() 함수와 동일한 기능을 구현해 봄
-        int Abs(int num) => (num < 0) ? -num : num;
+        int Abs(int number) => (number < 0) ? -number : number;
 
         //[1] Initialize
         int min = int.MaxValue; // 차잇값의 절댓값의 최솟값이 담길 그릇
 
-        //[2] Input: 2진수와 16진수로 표현 
-        int[] numbers = { 10, 20, 30, 27, 17 };
+        //[2] Input: 2진수와 16진수로 표현({ 10, 20, 30, 27, 17 }) 
+        int[] numbers = { 0b1010, 0x14, 0b11110, 0x1B, 0b10001 }; 
         int target = 25; // target과 가까운 값
-        int near = default; // 가까운값 : 27
+        int near = default; // 가까운 값 : 27
 
         //[3] Process: NEAR
         for (int i = 0; i < numbers.Length; i++)
@@ -39,3 +39,6 @@ class NearAlgorithm
         WriteLine($"{target}와 가장 가까운 값(문): {near}(차이: {min})");
     }
 }
+
+//numbers.First(n => Math.Abs(n - target) == numbers.Min(m => Math.Abs(m - target)))
+//27
