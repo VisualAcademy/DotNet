f4f72ed72ea51479c4ca04c94b522d75848fd319
RedPlus <redplus@redplus.net>
2019-08-02 20:21:30 +0900

    코드 정리

--- DotNet/DotNet/Algorithms/09_SearchAlgorithm/SearchAlgorithm.cs
+++ DotNet/DotNet/Algorithms/09_SearchAlgorithm/SearchAlgorithm.cs
@@ -1,38 +1,37 @@
-﻿//[?] 이분(이진) 검색(탐색): 반씩 나눠서 검색, 정렬이 되어있어야 함. 
+﻿//[?] 정렬되어 있는 데이터를 이진 검색(이분 탐색)을 사용하여 반씩 나눠서 검색
 using System;
 
 /// <summary>
-/// 검색 알고리즘(Search Algorithm): 주어진 데이터에서 특정 데이터를 찾기 
+/// 검색 알고리즘(Search Algorithm): 주어진 데이터에서 특정 데이터 찾기 
 /// </summary>
 class SearchAlgorithm
 {
     static void Main()
     {
-        //[1] Input: 정수 배열 대신에 List<int> 사용해볼 것: 많이 사용했기에...
-        int[] data = { 1, 3, 5, 7, 9 }; //[!] 오름차순 정렬되었다고 가정...
+        //[1] Input
+        int[] data = { 1, 3, 5, 7, 9 }; // 오름차순으로 정렬되었다고 가정
         int N = data.Length; // 의사코드
-        int search = 3; // 검색할 데이터: Console.ReadLine() 등으로 표현
-        bool flag = false; // 찾았으면 true 그렇지않으면 false 
-        int index = -1; // 찾은 위치
+        int search = 3; // 검색할 데이터: Console.ReadLine() 등으로 가져오기
+        bool flag = false; // 플래그 변수: 찾으면 true 찾지못하면 false 
+        int index = -1; // 인덱스 변수: 찾은 위치
 
-        // 이분탐색 관련 변수
-        //[2] Process: Binary Search: Full Scan -> Index Scan
+        //[2] Process: 이진 검색(Binary Search): Full Scan -> Index Scan
         int low = 0; // min: 낮은 인덱스
         int high = N - 1; // max: 높은 인덱스 
         while (low <= high)
         {
-            int mid = (low + high) / 2; // // 중간 인덱스 구하기
+            int mid = (low + high) / 2; // 중간 인덱스 구하기
             if (data[mid] == search)
             {
-                flag = true; index = mid; break;
+                flag = true; index = mid; break; // 찾으면 플래그, 인덱스 저장 후 종료
             }
             if (data[mid] < search)
             {
-                low = mid + 1;
+                low = mid + 1; // 찾을 데이터가 크면 오른쪽 영역으로 이동
             }
             else
             {
-                high = mid - 1;
+                high = mid - 1; // 찾을 데이터가 작으면 왼쪽 영역으로 이동 
             }
         }
 
