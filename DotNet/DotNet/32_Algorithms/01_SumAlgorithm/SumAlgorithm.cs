57f070a0b3e7cfc9d4db58349941cb060208d14d
RedPlus <redplus@redplus.net>
2019-08-18 12:50:06 +0900

    디버거 소개

--- DotNet/DotNet/32_Algorithms/01_SumAlgorithm/SumAlgorithm.cs
+++ DotNet/DotNet/32_Algorithms/01_SumAlgorithm/SumAlgorithm.cs
@@ -10,7 +10,7 @@ class SumAlgorithm
     {
         //[1] Input: n명의 점수
         int[] scores = { 100, 75, 50, 37, 90, 95 };
-        int sum = 0;
+        int sum = 0; // 합계가 담길 그릇
 
         //[2] Process: 합계 알고리즘 영역: 주어진 범위에 주어진 조건(필터링)
         for (int i = 0; i < scores.Length; i++) // 주어진 범위
@@ -23,6 +23,8 @@ class SumAlgorithm
 
         //[3] Output
         Console.WriteLine($"{scores.Length}명의 점수 중 80점 이상의 총점: {sum}");
+
+        //[!] 디버거(디버깅 S/W)를 사용하여 디버깅하기: F9 -> F5 -> F11 -> F5 
     }
 }
 
