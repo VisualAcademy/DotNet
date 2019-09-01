0f31ed2ea5c3b0214f29ba4b6bc69e092a3b1a86
RedPlus <redplus@redplus.net>
2019-09-02 08:49:20 +0900

    코드정리

---
+++ DotNet/DotNet/12_BitAndShift/RadixTransformation/RadixTransformation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+/// <summary>
+/// 진법변환
+/// </summary>
+class RadixTransformation
+{
+    static void Main()
+    {
+        // 10진수 10을 2진수로 변경하면 1010
+        Console.WriteLine($"10 : {Convert.ToString(10, 2)}"); // 8421 => 1010
+
+        // 2진수 1010을 10진수로 변경하면 10
+        Console.WriteLine($"1010 : {Convert.ToInt32("1010", 2)}"); // 10
+    }
+}
