04c549979f203a5966e694deb96c09be2b798788
RedPlus <redplus@redplus.net>
2019-09-10 07:18:59 +0900

    코드 정리

---
+++ DotNet/DotNet/69_Thread/ConcurrencyFor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+// 동시성(Concurrency)
+class ConcurrencyFor
+{
+    static void Main()
+    {
+        for (int i = 0; i < 200000; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ DotNet/DotNet/69_Thread/ParallelFor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+// 병렬 처리(Parallel Processing): 스레드를 직접 만들지 않고 다중 스레드로 처리
+class ParallelFor
+{
+    static void Main()
+    {
+        Parallel.For(0, 200000, (i) => { Console.WriteLine(i); });
+    }
+}
