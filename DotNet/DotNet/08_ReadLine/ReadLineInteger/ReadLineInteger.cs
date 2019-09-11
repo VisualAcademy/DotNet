c6dd650b273ed55ebddcf01183823a6ba2a40ed7
RedPlus <redplus@redplus.net>
2019-09-12 02:41:02 +0900

    8장 코드 정리

--- DotNet/DotNet/08_ReadLine/ReadLineInteger/ReadLineInteger.cs
+++ DotNet/DotNet/08_ReadLine/ReadLineInteger/ReadLineInteger.cs
@@ -7,6 +7,6 @@ class ReadLineInteger
         Console.Write("정수를 입력하세요: ");
         string input = Console.ReadLine(); // "10"을 입력한다면
         int number = Convert.ToInt32(input); // "10"을 정수 10으로 변환
-        Console.WriteLine(number); // 10
+        Console.WriteLine($"{number}-{number.GetType()}"); // 10-System.Int32
     }
 }
