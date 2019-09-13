412f4ea5b1fb482a58a11a53a3f97f0bf27cde13
RedPlus <redplus@redplus.net>
2019-09-14 01:19:08 +0900

    18장 코드 정리

--- DotNet/DotNet/18_WhileDoForEach/WhileDemo/WhileDemo.cs
+++ DotNet/DotNet/18_WhileDoForEach/WhileDemo/WhileDemo.cs
@@ -5,11 +5,11 @@ class WhileDemo
 {
     static void Main()
     {
-        int i = 1;
-        while (i <= 5)
+        int i = 1; // 초기식부터
+        while (i <= 5) // 조건식을 만족하는동안
         {
-            Console.WriteLine("카운트: {0}", i);
-            i++; 
+            Console.WriteLine("카운트: {0}", i); // 실행문을 실행하고
+            i++; // 증감식을 사용하여 인덱스 변수를 1씩증가 
         }
     }
 }
