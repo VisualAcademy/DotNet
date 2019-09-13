f0daa097a56f4f44997b0e1aa136b3bf9ea266e1
RedPlus <redplus@redplus.net>
2019-09-14 04:02:19 +0900

    33장 집필 소스 정리

---
+++ DotNet/DotNet/33_Object/ClassAndObject/ClassAndObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+//[1] ClassCode 이름의 클래스(Class) 만들기
+public class ClassCode
+{
+    // Empty
+}
+
+class ClassAndObject
+{
+    static void Main()
+    {
+        //[2] 특정 클래스로부터 objectCode1, objectCode2 이름의 개체(Object) 만들기
+        ClassCode objectCode1 = new ClassCode();
+        ClassCode objectCode2 = new ClassCode();
+    }
+}
