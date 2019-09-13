da674e38856d5458c7a1ccc96c4cab946f4d3d1e
RedPlus <redplus@redplus.net>
2019-09-14 02:59:14 +0900

    28장 집필 코드 정리

--- DotNet/DotNet/28_Exception/ExceptionDemo/ExceptionDemo.cs
+++ DotNet/DotNet/28_Exception/ExceptionDemo/ExceptionDemo.cs
@@ -9,8 +9,8 @@ class ExceptionDemo
             int[] arr = new int[2];
             arr[100] = 1234;
         }
-        catch (Exception ex)
-        {
+        catch (Exception ex) // ex 변수에는 예외에 대한 상세 정보가 담김
+        {            
             Console.WriteLine(ex.Message);
         }
     }
