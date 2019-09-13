13191d82af2ef3aa7a5dfc86fce20e0587c2c73b
RedPlus <redplus@redplus.net>
2019-09-14 03:06:00 +0900

    29장 집필 소스 정리

--- DotNet/DotNet/29_Collection/ArraySort/ArraySort.cs
+++ DotNet/DotNet/29_Collection/ArraySort/ArraySort.cs
@@ -5,7 +5,7 @@ class ArraySort
     static void Main()
     {
         int[] arr = { 3, 2, 1, 4, 5 };
-        Array.Sort(arr); // 정렬
+        Array.Sort(arr); // 오름차순 정렬: 1, 2, 3 순서
         foreach (var item in arr)
         {
             Console.WriteLine(item);            
