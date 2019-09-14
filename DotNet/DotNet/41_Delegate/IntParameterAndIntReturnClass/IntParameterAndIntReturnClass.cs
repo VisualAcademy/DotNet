64a6a024d0426fc989135296df3618f47dceb1a6
RedPlus <redplus@redplus.net>
2019-09-15 01:29:42 +0900

    41장 집필 소스 정리

---
+++ DotNet/DotNet/41_Delegate/IntParameterAndIntReturnClass/IntParameterAndIntReturnClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class IntParameterAndIntReturnClass
+{
+    delegate int IntParameterAndIntReturnDelegate(int x);
+
+    static void Main()
+    {
+        IntParameterAndIntReturnDelegate pow = (x) => x * x;
+        Console.WriteLine(pow(3));
+    }
+}
