bada6ed050b1f66e45541cf762ad9cce21955f3e
RedPlus <redplus@redplus.net>
2019-09-16 11:56:21 +0900

    집필 소스 정리중...

---
+++ DotNet/DotNet/08_ReadLine/IntParse/IntParse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class IntParse
+{
+    static void Main()
+    {
+        string strNumber = "1234";
+
+        int number1 = Convert.ToInt32(strNumber);
+        Console.WriteLine($"{number1} - {number1.GetType()}");
+
+        int number2 = int.Parse(strNumber);
+        Console.WriteLine($"{number2} - {number2.GetType()}");
+
+        int number3 = Int32.Parse(strNumber);
+        Console.WriteLine($"{number3} - {number3.GetType()}");
+    }
+}
