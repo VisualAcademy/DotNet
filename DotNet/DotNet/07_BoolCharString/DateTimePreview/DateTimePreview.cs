211904c87676645289ca22e304f13a79ac47db68
RedPlus <redplus@redplus.net>
2019-09-17 07:26:03 +0900

    집필 소스 정리

---
+++ DotNet/DotNet/07_BoolCharString/DateTimePreview/DateTimePreview.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class DateTimePreview
+{
+    static void Main()
+    {
+        //[1] 날짜 전체
+        Console.WriteLine(DateTime.Now);
+
+        //[2] 각각의 항목을 나눠서 출력
+        Console.WriteLine($"{DateTime.Now.Year}-{DateTime.Now.Month}-{DateTime.Now.Day}");
+        Console.WriteLine($"{DateTime.Now.Hour}:{DateTime.Now.Minute}-{DateTime.Now.Second}");
+    }
+}
---
+++ DotNet/DotNet/09_Operator/NumberToString/NumberToString.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class NumberToString
+{
+    static void Main()
+    {
+        int days = 28;
+
+        //[1] 암시적(Implicit)으로 숫자 형식을 문자열로 변환하기 
+        Console.WriteLine("2월달은 " + days + "일입니다.");
+
+        //[2] 명시적(Explicit)으로 숫자 형식을 문자열로 변환하기: 2가지 방법 
+        Console.WriteLine("2월달은 " + days.ToString() + "일입니다.");
+        Console.WriteLine("2월달은 " + Convert.ToString(days) + "일입니다.");
+    }
+}
