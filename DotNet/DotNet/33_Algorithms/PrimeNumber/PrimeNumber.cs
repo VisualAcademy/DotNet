49c439ea8e7352c34d8b558ba94802273487cb16
RedPlus <redplus@redplus.net>
2019-09-19 09:39:43 +0900

    소수(Prime Number)

---
+++ DotNet/DotNet/33_Algorithms/PrimeNumber/PrimeNumber.cs
@@ -0,0 +1,37 @@
+﻿//[?] 특정 수를 입력 받아서, 소수인지 아닌지 판별하는 프로그램
+// - 5는 소수: 1과 5만을 약수로 가짐
+// - 6은 소수가 아님: 1과 6만이 아닌 추가로 2와 3을 약수로 가짐 
+// - 2부터 해당 수까지 나머지값을 구해서 나머지값이 0일때 나눈 수가 해당 수라면 소수 
+using System;
+
+/// <summary>
+/// 소수(Prime Number): 자연수 중에서 1과 자기 자신만을 약수로 가지는 자연수
+/// </summary>
+class PrimeNumber
+{
+    static void Main()
+    {
+        //[1] Input
+        var number = 0;
+        Console.Write("수 입력: _\b");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        //[2] Process: Prime Number: 2부터 n까지 나누어 떨어지는 수가 발생할 때가지 반복
+        var i = 1;
+        do
+        {
+            i = i + 1; // 2부터 n까지 비교            
+            Console.WriteLine($"{number} % {i} = {number % i}");
+        } while (number % i != 0); // 소수는 number로만 나누어 떨어짐
+
+        //[3] Output
+        if (number == i)
+        {
+            Console.WriteLine("소수임");
+        }
+        else
+        {
+            Console.WriteLine("소수 아님");
+        }
+    }
+}
---
+++ DotNet/DotNet/33_Algorithms/PrimeNumberCount/PrimeNumberCount.cs
@@ -0,0 +1,48 @@
+﻿//[?] 정수 하나 입력 받아서, 2부터 해당 수까지 안에 존재하는 모든 소수를 찾아서 출력
+// 예: 2 ~ 20까지의 정수: 2, 3, 5, 7, 11, 13, 17, 19 (8개)
+using System;
+
+/// <summary>
+/// 소수(Prime Number) 개수(Count) 구하기 
+/// </summary>
+class PrimeNumberCount
+{
+    static void Main()
+    {
+        int count = 0; // 소수 개수
+        bool sw = true; // 소수(true)인지를 확인하는 스위치 변수(flag)
+
+        int number = 0; // 2 ~ number까지의 수 중 소수를 구함
+        Console.Write("수 입력: _\b");
+        number = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 2; i <= number; i++) // 2부터 n까지 반복하면서 소수 판별
+        {
+            sw = true; // 일단 모든 반복마다 소수로 놓고 시작
+
+            // 2부터 현재수(i) - 1까지 나누었을 때 나누어 떨어지면 소수가 아님
+            for (int j = 2; j < i; j++)
+            {
+                if (i % j == 0)
+                {
+                    sw = false;
+                    break;
+                }
+            }
+
+            if (sw)
+            {
+                Console.Write("{0}\t", i);
+                count++;
+
+                // 줄바꿈
+                if (count % 5 == 0)
+                {
+                    Console.WriteLine();
+                }
+            }
+        }
+
+        Console.WriteLine("\n2부터 {0}까지의 소수의 개수: {1}", number, count);
+    }
+}
