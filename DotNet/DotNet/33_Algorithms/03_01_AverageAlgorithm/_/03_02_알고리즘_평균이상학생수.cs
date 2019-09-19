2e7cf4c60682c061ce4dc18b3be4daf0e4cab866
RedPlus <redplus@redplus.net>
2019-09-19 10:50:48 +0900

    평균 이상 학생수 구하기

--- DotNet/DotNet/33_Algorithms/03_01_AverageAlgorithm/_/03_02_알고리즘_평균이상학생수.cs
+++ DotNet/DotNet/33_Algorithms/03_01_AverageAlgorithm/_/03_02_알고리즘_평균이상학생수.cs
@@ -2,39 +2,36 @@
 // 평균(합계, 카운트) -> 카운트
 using System;
 
-namespace 알고리즘_평균이상학생수
+class 알고리즘_평균이상학생수
 {
-    class 알고리즘_평균이상학생수
+    static void Main()
     {
-        static void Main(string[] args)
+        int[] t = { 100, 90, 80, 70, 60 };
+
+        int sum = 0; // 총점            
+        double avg = 0.0; // 평균
+
+        int count = 0; // 학생수(평균 이상)
+
+        // 합계 알고리즘
+        for (int i = 0; i < t.Length; i++)
         {
-            int[] t = { 100, 90, 80, 70, 60 };
-
-            int sum = 0; // 총점            
-            double avg = 0.0; // 평균
-
-            int count = 0; // 학생수(평균 이상)
-
-            // 합계 알고리즘
-            for (int i = 0; i < t.Length; i++)
-            {
-                sum += t[i]; // 누적
-            }
-            // 평균 알고리즘
-            avg = sum / (double)t.Length;
-
-            Console.WriteLine("평균: {0}", avg);
-
-            // 필터링이 적용된 카운트 알고리즘
-            for (int i = 0; i < t.Length; i++)
-            {
-                if (t[i] >= avg) // 평균 이상인 학생수
-                {
-                    count++; // 카운트 알고리즘 적용
-                }
-            }
-
-            Console.WriteLine("평균 이상 점수 학생 수: {0}", count);
+            sum += t[i]; // 누적
         }
+        // 평균 알고리즘
+        avg = sum / (double)t.Length;
+
+        Console.WriteLine("평균: {0}", avg);
+
+        // 필터링이 적용된 카운트 알고리즘
+        for (int i = 0; i < t.Length; i++)
+        {
+            if (t[i] >= avg) // 평균 이상인 학생수
+            {
+                count++; // 카운트 알고리즘 적용
+            }
+        }
+
+        Console.WriteLine("평균 이상 점수 학생 수: {0}", count);
     }
 }
---
+++ DotNet/DotNet/33_Algorithms/AverageCount/AverageCount.cs
@@ -0,0 +1,42 @@
+﻿//[?] 전체 학생 평균 이상의 성적인 학생의 수를 구하는 프로그램
+using System;
+
+/// <summary>
+/// 평균 이상 학생수 구하기: 평균(합계, 개수) -> 개수 
+/// </summary>
+class AverageCount
+{
+    static void Main()
+    {
+        //[1] Input
+        int[] scores = { 100, 90, 80, 70, 60 };
+        var sum = 0; // 총점
+        var count = 0; // 개수
+        var avg = 0.0; // 평균
+        var resultCount = 0; // 평균 이상 학생수 
+
+        //[2] Process: SUM, COUNT, AVERAGE -> COUNT
+        //[2][1] AVERAGE
+        for (int i = 0; i < scores.Length; i++)
+        {
+            sum += scores[i]; // SUM
+            count++; // COUNT
+        }
+        if (sum != 0 && count != 0)
+        {
+            avg = sum / (double)count; // AVERAGE            
+        }
+
+        //[2][2] 평균 이상 학생수
+        for (int i = 0; i < scores.Length; i++)
+        {
+            if (scores[i] >= avg) // 평균 이상인 학생수
+            {
+                resultCount++; // 또 다른 COUNT 알고리즘
+            }
+        }
+
+        //[3] Output
+        Console.WriteLine($"평균({avg}점) 이상을 받은 학생수: {resultCount}명");
+    }
+}
