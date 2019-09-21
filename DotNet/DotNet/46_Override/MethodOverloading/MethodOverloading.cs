6a03ba6b98e9706e78ce8943b9ebe3ce62d6aa27
RedPlus <redplus@redplus.net>
2019-09-21 23:54:50 +0900

    코드 정리

--- DotNet/DotNet/46_Override/MethodOverloading/MethodOverloading.cs
+++ DotNet/DotNet/46_Override/MethodOverloading/MethodOverloading.cs
@@ -4,7 +4,7 @@ class MethodOverloading
 {
     static void Main()
     {
-        int number = 100;
+        var number = 100;
         Print(number);      // 100
         Print(ref number);  // 101
         Print(number);      // 101
