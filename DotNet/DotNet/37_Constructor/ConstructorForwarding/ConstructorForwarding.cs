5b12da55a04a36350b98f91a22c5590b066a1d22
RedPlus <redplus@redplus.net>
2019-09-23 17:36:58 +0900

    코드 정리

--- DotNet/DotNet/37_Constructor/ConstructorForwarding/ConstructorForwarding.cs
+++ DotNet/DotNet/37_Constructor/ConstructorForwarding/ConstructorForwarding.cs
@@ -4,16 +4,16 @@ namespace ConstructorForwarding
 {
     class Money
     {
-        public Money() : this(1000) { }
-        public Money(int money) => Console.WriteLine("월급: {0}", money);
+        public Money() : this(1000) { } // 아래 생성자로 전송
+        public Money(int money) => Console.WriteLine("Money: {0:#,###}", money);
     }
 
     class ConstructorForwarding
     {
         static void Main()
         {
-            var basic = new Money();        // 1000
-            var bonus = new Money(2000);    // 2000
+            var basic = new Money(); // 1000
+            var bonus = new Money(2000); // 2000
         }
     }
 }
