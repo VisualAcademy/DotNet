01bbb06009a99fd3b10da2cad27813f159479448
RedPlus <redplus@redplus.net>
2019-09-23 21:49:56 +0900

    집필 소스 파일 추가

---
+++ DotNet/DotNet/42_Delegate/DelegateGetArea/DelegateGetArea.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class DelegateGetArea
+{
+    static double GetArea(int r) => 3.14 * r * r;
+
+    delegate double GetAreaPointer(int r);
+
+    static void Main()
+    {
+        GetAreaPointer pointer = GetArea;
+        Console.WriteLine(pointer.Invoke(10));
+        Console.WriteLine(pointer(10));
+    }
+}
