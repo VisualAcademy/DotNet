e74d9a154a7b55e9528b027e31dc8f021b5f3dc6
RedPlus <redplus@redplus.net>
2019-09-25 01:14:05 +0900

    폴더 및 코드 정리

--- DotNet/DotNet/14_If/StringCompare/StringCompare.cs
+++ DotNet/DotNet/14_If/StringCompare/StringCompare.cs
@@ -4,12 +4,12 @@ class StringCompare
 {
     static void Main()
     {
-        string s1 = "안녕하세요.";
-        string s2 = "안녕하세요.";
+        string s1 = "Hello.";
+        string s2 = "Hello.";
 
         if (s1 == s2) // s1과 s2가 같으면 true
         {
-            Console.WriteLine("같다.");
+            Console.WriteLine("Same.");
         }
     }
 }
