b42cf9374069859e6a6e8a768bce10657e6e2046
RedPlus <redplus@redplus.net>
2019-09-29 09:26:52 +0900

    코드 정리

--- DotNet/DotNet/11_RelationalLogical/OperatorRelational/OperatorRelational.cs
+++ DotNet/DotNet/11_RelationalLogical/OperatorRelational/OperatorRelational.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// 관계형 연산자(비교 연산자;Relational Operator, Comparative Operator)
+// 관계형 연산자: < 작음, <= 작거나 같음, > 큼, >= 크거나 같음, == 같음, != 다름
+using System;
 
 class OperatorRelational
 {
