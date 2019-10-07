cf47da36dadf9b281be3965b81a236ca89569cf3
RedPlus <redplus@redplus.net>
2019-10-08 06:05:40 +0900

    코드 정리

--- DotNet/DotNet/30_GenericClass/ListOfString/ListOfString.cs
+++ DotNet/DotNet/30_GenericClass/ListOfString/ListOfString.cs
@@ -6,7 +6,7 @@ class ListOfString
 {
     static void Main()
     {
-        //[0] 제네릭 리스트 사용을 위한 인스턴스 생성
+        //[0] 제네릭 리스트 사용을 위한 인스턴스 생성 및 샘플 데이터 입력
         List<string> colors = new List<string>();
         colors.Add("Red");
         colors.Add("Green");
@@ -15,13 +15,15 @@ class ListOfString
         //[1] for 문으로 출력 예
         for (int i = 0; i < colors.Count; i++)
         {
-            Console.WriteLine(colors[i]);
+            Console.Write($"{colors[i]}\t");
         }
+        Console.WriteLine();
 
         //[2] foreach 문으로 출력 예
         foreach (var color in colors)
         {
-            Console.WriteLine(color);
+            Console.Write($"{color}\t");
         }
+        Console.WriteLine();
     }
 }
