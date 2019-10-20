6a8a97643b647157f678f98ab431f6b5deb8e032
RedPlus <redplus@redplus.net>
2019-10-20 20:17:45 +0900

    코드 정리

--- DotNet/DotNet/34_Object/StaticAndInstance/StaticAndInstance.cs
+++ DotNet/DotNet/34_Object/StaticAndInstance/StaticAndInstance.cs
@@ -7,18 +7,20 @@ namespace StaticAndInstance
     class SharedAndInstance
     {
         //[1][1] static(shared) 멤버
-        public static void StaticMember() => Console.WriteLine("[1] 정적 멤버");
+        public static void StaticMember() => Console.WriteLine("[1] Static Member");
+
         //[1][2] instance 멤버
-        public void InstanceMember() => Console.WriteLine("[2] 인스턴스 멤버");
+        public void InstanceMember() => Console.WriteLine("[2] Instance Member");
     }
 
     class StaticAndInstance
     {
         static void Main()
         {
-            //[2] 클래스
+            //[2] 클래스 사용
             //[2][1] 정적 멤버 사용
             SharedAndInstance.StaticMember(); // 정적 멤버 => 클래스.멤버 형태
+
             //[2][2] 인스턴스 멤버 사용
             SharedAndInstance obj = new SharedAndInstance();
             obj.InstanceMember(); // 인스턴스 멤버 => 개체.멤버 형태
