ad0c45caf8fc429de7cc2e1254dff38a882a4e8b
RedPlus <redplus@redplus.net>
2019-11-01 00:04:36 +0900

    집필 코드 정리

--- DotNet/DotNet/04_Syntax/PlaceHolderTwice/PlaceHolderTwice.cs
+++ DotNet/DotNet/04_Syntax/PlaceHolderTwice/PlaceHolderTwice.cs
@@ -5,6 +5,6 @@ class PlaceHolderTwice
     static void Main()
     {
         Console.WriteLine("{0}", "Hello, C#");
-        Console.WriteLine("{0} {0}", "Hello, C#");
+        Console.WriteLine("{0}, {0}", "Hello, C#");
     }
 }
