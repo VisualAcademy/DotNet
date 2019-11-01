d6b383581fad8f2b068d7ff4f9eb46963699fd7e
RedPlus <redplus@redplus.net>
2019-11-01 19:55:56 +0900

    코드 정리

---
+++ DotNet/DotNet/64_Async/AsyncAwaitWithTask/AsyncAwaitWithTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+
+class AsyncAwaitWithTask
+{
+    //[1] 초간단 async와 await를 사용하는 메서드 만들기
+    static async Task RunAsync()
+    {
+        string message = "Async";
+        await Task.Delay(1);
+        Console.WriteLine(message);
+    }
+
+    static async Task Main()
+    {
+        //[2] 비동기 메서드 호출
+        await RunAsync();
+    }
+}
