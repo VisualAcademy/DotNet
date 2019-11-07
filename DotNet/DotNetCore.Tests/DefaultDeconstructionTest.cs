8449bb608c96d73c86d8d1bd96f7d36c7766f697
RedPlus <redplus@redplus.net>
2019-11-08 03:37:25 +0900

    코드 정리

---
+++ DotNet/DotNetCore.Tests/DefaultDeconstructionTest.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DotNetCore.Tests
+{
+    [TestClass]
+    public class DefaultDeconstructionTest
+    {
+        [TestMethod]
+        public void DefaultTest()
+        {
+            (int number, string? name) = (default, default);
+
+            Assert.AreEqual(0, number);
+            Assert.IsNull(name);
+        }
+    }
+}
---
+++ DotNet/DotNetCore.Tests/NullableReferenceTypeTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace DotNetCore.Tests
+{
+    class Person
+    {
+        public string Name { get; set; } 
+    }
+
+    [TestClass]
+    public class NullableReferenceTypeTest
+    {
+        [TestMethod]
+        public void NullableEnableTest()
+        {
+            Console.WriteLine((new Person()).Name);
+        }
+    }
+}
