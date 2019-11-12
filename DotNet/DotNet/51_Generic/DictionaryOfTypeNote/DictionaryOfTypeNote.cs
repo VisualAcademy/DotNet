337ad14a156b356a06975b4c70aed949aa3305a7
RedPlus <redplus@redplus.net>
2019-11-12 23:20:51 +0900

    코드 정리

--- DotNet/DotNet/51_Generic/DictionaryOfTypeNote/DictionaryOfTypeNote.cs
+++ DotNet/DotNet/51_Generic/DictionaryOfTypeNote/DictionaryOfTypeNote.cs
@@ -7,20 +7,20 @@ namespace DotNet
     {
         static void Main()
         {
-            // [1] 키와 값으로 이루어진 Dictionary<T> 개체 생성
+            //[1] 키와 값으로 이루어진 Dictionary<T> 개체 생성
             Dictionary<int, double> keyValuePairs = new Dictionary<int, double>();
 
-            // [2] 1부터 12까지 기본값으로 초기화: 배열과 달리 1부터 12까지 지정 가능
+            //[2] 1부터 12까지 기본값으로 초기화: 배열과 달리 1부터 12까지 지정 가능
             for (int i = 1; i <= 12; i++)
             {
                 keyValuePairs.Add(i, 0.0); 
             }
 
-            // [3] 월별 전기 요금 사용량 관리
+            //[3] 월별 전기 요금 사용량 관리
             keyValuePairs[1] = 10.01; // 1월에 10.01 kW 사용했다고 가정
             keyValuePairs[2] = 20.02; // 2월에 20.02 kW 사용했다고 가정
 
-            // [4] 월별 사용량 출력
+            //[4] 월별 사용량 출력
             for (int i = 1; i <= 3; i++)
             {
                 Console.WriteLine($"{i}월 - {keyValuePairs[i]}kW 사용");
