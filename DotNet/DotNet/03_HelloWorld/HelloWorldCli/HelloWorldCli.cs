8ee4a613b6cc3afef2f4296e582a8f2955aab5c2
RedPlus <redplus@redplus.net>
2019-11-15 01:30:52 +0900

    Hello World만 100분 강의

---
+++ DotNet/DotNet/03_HelloWorld/HelloWorldCli/HelloWorldCli.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! with Visual Studio Code");
+        }
+    }
+}
---
+++ DotNet/DotNet/03_HelloWorld/HelloWorldCore/HelloWorldCore.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldCore
+{
+    public class HelloWorldCore
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World Core");
+        }
+    }    
+}
