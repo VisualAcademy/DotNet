09220b86cc4262930bca6273edfe4b740fda0269
RedPlus <redplus@redplus.net>
2019-11-17 03:19:09 +0900

    코드 정리

---
+++ DotNet/DotNet/52_Dynamic/DynamicBinding/DynamicBinding.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class DynamicBinding
+{
+    static void Main()
+    {
+        //[?] 동적 바인딩
+        dynamic now = DateTime.Now; 
+        int hour = now.Hour;
+        Console.WriteLine(hour);
+        Console.WriteLine(now.NoProperty); // 런타임 시점에 에러 발생
+    }
+}
