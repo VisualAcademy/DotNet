1b46deb887645d4677dcdd265f251d80e6b5a2be
RedPlus <redplus@redplus.net>
2019-11-17 03:35:08 +0900

    코드 정리

---
+++ DotNet/DotNet/52_Dynamic/DynamicExtensionMethod/DynamicExtensionMethod.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+static class DynamicExtensionMethod
+{
+    // 확장 메서드 
+    static string PreDotNet(this string str) => $"....{str}";
+
+    static void Main()
+    {
+        string s1 = "DotNet";
+        Console.WriteLine(s1.PreDotNet()); //[1] 실행됨
+
+        dynamic d1 = "Korea";
+        Console.WriteLine(d1.PreDotNet()); //[2] 에러 발생
+    }
+}
