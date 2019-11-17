15029203c937d1cda06c2aa1fd4352b882223662
RedPlus <redplus@redplus.net>
2019-11-18 02:42:56 +0900

    코드 정리

--- DotNet/DotNet/64_Async/AsyncAwaitSimple/AsyncAwaitSimple.cs
+++ DotNet/DotNet/64_Async/AsyncAwaitSimple/AsyncAwaitSimple.cs
@@ -4,12 +4,12 @@ using System.Threading.Tasks;
 
 class AsyncAwaitSimple
 {
-    // [1] 비동기 메서드를 호출하는 DoAsync() 메서드 생성시 async 키워드 붙임
+    //[1] 비동기 메서드를 호출하는 DoAsync() 메서드 생성시 async 키워드 붙임
     static async Task DoAsync()
     {
         using (var client = new HttpClient())
         {
-            // [2] .NET API의 비동기 메서드 호출할 때 await 키워드 붙임
+            //[2] .NET API의 비동기 메서드 호출할 때 await 키워드 붙임
             var r = await client.GetAsync("http://dotnetnote.com/api/WebApiDemo");
             var c = await r.Content.ReadAsStringAsync();
 
@@ -17,10 +17,10 @@ class AsyncAwaitSimple
         }
     }
 
-    // [3] Main() 메서드를 async 키워드를 붙여 비동기 메서드로 변경
+    //[3] Main() 메서드를 async 키워드를 붙여 비동기 메서드로 변경
     static async Task Main()
     {
-        // [4] 비동기 메서드 호출할 때 await 키워드를 앞에 붙임
+        //[4] 비동기 메서드 호출할 때 await 키워드를 앞에 붙임
         await DoAsync();
     }
 }
