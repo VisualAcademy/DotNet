bf42b2419ce8e6d0e5a24a2b40a0690c53be2ed3
RedPlus <redplus@redplus.net>
2019-11-24 18:24:25 +0900

    코드정리

---
+++ DotNet/DotNet/32_LINQ/LinqAggregate/LinqAggregate.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+class LinqAggregate
+{
+    static void Main()
+    {
+        //[1] Aggregate 확장 메서드로 최댓값 구하기
+        int[] numbers = { 1, 2, 3, 4, 5 };
+        int max = numbers.Aggregate((f, s) => f > s ? f : s);
+        Console.WriteLine(max);
+
+        //[2] Aggregate 확장 메서드로 최솟값 구하기
+        int min = numbers.Aggregate((c, n) => c < n ? c : n);
+        Console.WriteLine(min);
+    }
+}
---
+++ DotNet/DotNet/63_Functional/AggregateDemo/AggregateDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+class AggregateDemo
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        //[1] Aggregate 확장 메서드로 최댓값 구하기
+        int max = numbers.Aggregate((f, s) => f > s ? f : s);
+        Console.WriteLine(max);
+
+        //[2] Aggregate 확장 메서드로 최솟값 구하기
+        int min = numbers.Aggregate((c, n) => c < n ? c : n);
+        Console.WriteLine(min);
+    }
+}
