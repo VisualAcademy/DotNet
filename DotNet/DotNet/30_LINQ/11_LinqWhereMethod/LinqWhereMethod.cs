08db067f35fbbbfd5ff7228cdde9519e8f6f75f6
RedPlus <redplus@redplus.net>
2020-02-27 03:02:02 +0900

    30장 정리

--- DotNet/DotNet/30_LINQ/11_LinqWhereMethod/LinqWhereMethod.cs
+++ DotNet/DotNet/30_LINQ/11_LinqWhereMethod/LinqWhereMethod.cs
@@ -7,7 +7,7 @@ class LinqWhereMethod
     {
         int[] arr = { 1, 2, 3, 4, 5 };
 
-        // 배열에서 홀수만 추출: goes to 연산자 사용
+        // 배열에서 홀수만 추출: 람다 식 사용
         var q = arr.Where(num => num % 2 == 1);
 
         foreach (var n in q)
