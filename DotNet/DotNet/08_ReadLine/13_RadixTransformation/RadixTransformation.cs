405fd8ac10a49eb22d9ece49a9ed445125b494d8
RedPlus <redplus@redplus.net>
2020-03-01 11:42:26 +0900

    08장 정리

---
+++ DotNet/DotNet/08_ReadLine/13_RadixTransformation/RadixTransformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+/// 진법변환
+/// </summary>
+class RadixTransformation
+{
+    static void Main()
+    {
+        // 10진수 10을 2진수로 변경하면 1010
+        Console.WriteLine(
+            $"십진수 10을 이진수로 변경: {Convert.ToString(10, 2)}"); // 8421 => 1010
+
+        // 2진수 1010을 10진수로 변경하면 10
+        Console.WriteLine(
+            $"이진수 1010을 십진수로 변경: {Convert.ToInt32("1010", 2)}"); // 10
+    }
+}
