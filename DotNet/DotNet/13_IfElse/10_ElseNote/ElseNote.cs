9859b7af6f956c990573eeac07d77fad71f2fecc
RedPlus <redplus@redplus.net>
2020-03-01 17:13:00 +0900

    코드 정리

--- DotNet/DotNet/13_IfElse/10_ElseNote/ElseNote.cs
+++ DotNet/DotNet/13_IfElse/10_ElseNote/ElseNote.cs
@@ -6,7 +6,7 @@ class ElseNote
     {
         if (1 != 1)
         {
-            //Console.WriteLine("조건식이 거짓이기에 현재 문장이 실행되지 않습니다.");
+            //Console.WriteLine("조건식이 참이기에 현재 문장이 실행됩니다.");
         }
         else
         {
