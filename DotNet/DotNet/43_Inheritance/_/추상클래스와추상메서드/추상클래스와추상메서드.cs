0931da8c94eaa8931511a1e91b8bc22eb2b7eda4
RedPlus <redplus@redplus.net>
2020-04-17 05:31:08 +0900

    코드 정리

--- DotNet/DotNet/43_Inheritance/_/추상클래스와추상메서드/추상클래스와추상메서드.cs
+++ DotNet/DotNet/43_Inheritance/_/추상클래스와추상메서드/추상클래스와추상메서드.cs
@@ -1,15 +1,15 @@
-﻿// 추상(Abstract) 클래스 : 다른 클래스에게 상속을 주기 위한 클래스의 명세서 역할을 하는 클래스
+﻿// 추상(Abstract) 클래스: 다른 클래스에게 상속을 주기 위한 클래스의 명세서 역할을 하는 클래스
 // 클래스의 대한 설계서 역할, 회사내의 표준 정의(반드시 그 이름으로 무엇이가 기능 구현)
 using System;
 
 namespace 추상클래스와추상메서드
 {
     // 부모, Parent, Base, Super 클래스
-    public abstract class Benz
+    public abstract class Benz // Hyundai
     {
         // 일반 멤버
         public void Run() { Console.WriteLine("달리다."); }
-        // 추상 메서드 : 자식 클래스에게 강제적으로 반드시 구현해라~
+        // 추상 메서드: 자식 클래스에게 강제적으로 반드시 구현해라~
         public abstract void Left(); // 좌회전 기능의 프로토타입
         public abstract void Right(string msg);
     }
