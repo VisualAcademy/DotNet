99ca45bcd25e54362b8257ef32664d4c75b53377
RedPlus <redplus@redplus.net>
2020-06-11 04:14:24 +0900

    코드 정리

--- DotNet/DotNet/07_BoolCharString/07_StringInterpolationDemo/StringInterpolationDemo.cs
+++ DotNet/DotNet/07_BoolCharString/07_StringInterpolationDemo/StringInterpolationDemo.cs
@@ -8,14 +8,14 @@ class StringInterpolationDemo
         string name = "C#";
         string version = "8.0";
 
-        // WriteLine() 메서드의 기본 제공 기능 사용
+        //[1] WriteLine() 메서드의 기본 제공 기능 사용
         Console.WriteLine("{0} {1}", name, version);
 
-        // String.Format() 메서드 사용
+        //[2] String.Format() 메서드 사용
         string result = String.Format("{0} {1}", name, version);
         Console.WriteLine(result);
 
-        // 문자열 보간법(String Interpolation) 사용
+        //[3] 문자열 보간법(String Interpolation) 사용
         Console.WriteLine($"{name} {version}");
 
         // C# Interactive에서는 Console.WriteLine() 생략 가능
