30cae950029c0f18879815707698cfaab429b59a
RedPlus <redplus@redplus.net>
2020-06-26 06:43:10 +0900

    FunctionNote.cs 추가

---
+++ DotNet/DotNet/19_Function/FunctionNote.cs
@@ -0,0 +1,15 @@
+﻿using System; 
+
+class FunctionNote
+{
+    // 선언
+    static int Sum(int first, int second = 5) => (first + second);
+
+    // 호출: DotNet.exe 3 5 
+    static void Main(string[] args)
+    {
+        int first = Convert.ToInt32(args[0]);
+        int second = Convert.ToInt32(args[1]);
+        Console.WriteLine(Sum(first, second));    
+    } 
+}
