d4388da8de8c9225ce0b0e7cc9becf1e92110c38
RedPlus <redplus@redplus.net>
2020-06-30 06:42:23 +0900

    EnumerationTextbook.cs 열거형 사용 데모

---
+++ DotNet/DotNet/23_Enum/EnumerationTextbook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class EnumerationTextbook
+{
+    static void Main()
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Red");
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.BackgroundColor = ConsoleColor.White;
+        Console.WriteLine($"{nameof(ConsoleColor.Yellow)} & {nameof(ConsoleColor.White)}");
+        Console.ResetColor();
+    }
+}
