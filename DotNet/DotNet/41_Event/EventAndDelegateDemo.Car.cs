3147c9aaeb1bb4d21d55bfd87ae82774bea9db06
RedPlus <redplus@redplus.net>
2020-07-21 02:44:56 +0900

    Gauge 오탈자 수정

--- DotNet/DotNet/41_Event/EventAndDelegateDemo.Car.cs
+++ DotNet/DotNet/41_Event/EventAndDelegateDemo.Car.cs
@@ -5,30 +5,30 @@ namespace EventAndDelegateDemo
     // 이벤트 게시자(Publisher)
     class Car
     {
-        private int _fuelGuage;
-        public int FuleGuage
+        private int _fuelGauge;
+        public int FuleGauge
         {
-            get { return _fuelGuage; }
+            get { return _fuelGauge; }
             set 
             { 
-                _fuelGuage = value; OnFuelEmptyReached();
+                _fuelGauge = value; OnFuelEmptyReached();
             }
         }
         public Car()
         {
-            _fuelGuage = 25; // 25% 
+            _fuelGauge = 25; // 25% 
         }
         public void Go()
         {
-            Console.WriteLine("운전"); FuleGuage -= 5; // 5% 
+            Console.WriteLine("운전"); FuleGauge -= 5; // 5% 
         }
         //public delegate void FuelEmptyNotification();
         //public event FuelEmptyNotification FuelEmptyReached;
         public event Action FuelEmptyReached;
         public void OnFuelEmptyReached()
         {
-            Console.WriteLine($"연료 상태: {_fuelGuage}%");
-            if (_fuelGuage < 20)
+            Console.WriteLine($"연료 상태: {_fuelGauge}%");
+            if (_fuelGauge < 20)
             {
                 if (FuelEmptyReached != null)
                 {
