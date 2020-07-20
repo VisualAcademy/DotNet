e5a75bcfe4ecf5b5240b367363d1f384d209d654
RedPlus <redplus@redplus.net>
2020-07-21 02:11:23 +0900

    41_이벤트와 이벤트 처리기 사용 데모

---
+++ DotNet/DotNet/41_Event/EventAndDelegate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace EventAndDelegateDemo
+{
+    // 이벤트 구독자(Subscriber)
+    class EventAndDelegate
+    {
+        static void Main()
+        {
+            Car car = new Car();
+            car.FuelEmptyReached += Car_FuelEmptyReached;
+            car.FuelEmptyReached -= Car_FuelEmptyReached;
+            car.FuelEmptyReached += () => 
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.WriteLine("연료부족!!!");
+                Console.ResetColor();
+            };
+            car.Go();
+            //car.OnFuelEmptyReached();
+            car.Go();
+        }
+
+        // 이벤트 처리기(핸들러): Handler
+        private static void Car_FuelEmptyReached() =>
+            Console.WriteLine("연료 부족");
+    } 
+}
---
+++ DotNet/DotNet/41_Event/EventAndDelegateDemo.Car.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace EventAndDelegateDemo
+{
+    // 이벤트 게시자(Publisher)
+    class Car
+    {
+        private int _fuelGuage;
+        public int FuleGuage
+        {
+            get { return _fuelGuage; }
+            set 
+            { 
+                _fuelGuage = value; OnFuelEmptyReached();
+            }
+        }
+        public Car()
+        {
+            _fuelGuage = 25; // 25% 
+        }
+        public void Go()
+        {
+            Console.WriteLine("운전"); FuleGuage -= 5; // 5% 
+        }
+        //public delegate void FuelEmptyNotification();
+        //public event FuelEmptyNotification FuelEmptyReached;
+        public event Action FuelEmptyReached;
+        public void OnFuelEmptyReached()
+        {
+            Console.WriteLine($"연료 상태: {_fuelGuage}%");
+            if (_fuelGuage < 20)
+            {
+                if (FuelEmptyReached != null)
+                {
+                    FuelEmptyReached?.Invoke(); // FuelEmptyReached();
+                } 
+            }
+        }
+    }
+}
