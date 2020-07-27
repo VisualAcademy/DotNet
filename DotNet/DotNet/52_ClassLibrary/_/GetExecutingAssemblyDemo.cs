2265fd075244208b211e4c51cfa767a207a56d17
RedPlus <redplus@redplus.net>
2020-07-27 09:33:39 +0900

    코드 정리

---
+++ DotNet/DotNet/52_ClassLibrary/_/GetExecutingAssemblyDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+
+namespace GetExecutingAssemblyDemo
+{
+    class GetExecutingAssemblyDemo
+    {
+        static void Main(string[] args)
+        {
+            var t = Assembly.GetExecutingAssembly().GetTypes();
+            Console.WriteLine($"전체 형식 개수: {t.Length}");
+        }
+    }
+}
