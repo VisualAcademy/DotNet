d3bd6b83e6a6b30e93b2731bc78a4249525853ed
RedPlus <redplus@redplus.net>
2020-07-30 05:12:00 +0900

    확장 메서드 요약 설명

---
+++ DotNet/DotNet/49_ExtensionMethod/MyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MyExtensions
+{
+    static class StringExtension
+    {
+        public static string Ten(this string msg) => msg.Substring(0, 10);
+        public static string Five(this string msg) => msg.Substring(0, 5);
+        public static string AddElipsis(this string msg) => msg + "...";
+        public static string AddElipsis(this string msg, string elipsis) 
+            => $"{msg}{elipsis}";
+    }
+
+    class MyExtensionsDemo
+    {
+        static void Main()
+        {
+            string message = "안녕하세요. 반갑습니다. 또 만나요.";
+            Console.WriteLine(message.Ten().Five().AddElipsis());
+            Console.WriteLine(message.Ten().Five().Five().AddElipsis("___"));
+        }
+    }
+}
