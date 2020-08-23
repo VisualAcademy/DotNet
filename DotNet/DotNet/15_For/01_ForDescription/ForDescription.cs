48faf68308857c2787b345c3a9740636e87e2fc1
RedPlus <redplus@redplus.net>
2020-08-24 05:33:41 +0900

    ForPseudo

--- DotNet/DotNet/15_For/01_ForDescription/ForDescription.cs
+++ DotNet/DotNet/15_For/01_ForDescription/ForDescription.cs
@@ -7,7 +7,7 @@ class ForDescription
         // for (초기식;조건식;증감식) { }
         for (int i = 0; i < 3; i++) // i가 0, 1, 2일 때 
         {
-            Console.WriteLine("안녕하세요."); // 안녕하세요 출력
+            Console.WriteLine("안녕하세요."); // 안녕하세요. 출력
         }
     }
 }
