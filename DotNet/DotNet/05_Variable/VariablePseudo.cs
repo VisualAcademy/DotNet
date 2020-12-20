401f296014c018980b539f8fa3751da92a90c637
RedPlus <redplus@redplus.net>
2020-12-21 01:28:40 +0900

    VariablePseudo

---
+++ DotNet/DotNet/05_Variable/VariablePseudo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class VariablePseudo
+{
+    static void Main()
+    {
+        // 변수(Variable): 프로그램에서 사용할 데이터를 임시로 저장해 놓는 그릇
+        int age;
+        age = 21;
+        Console.WriteLine(age);
+    }
+}
