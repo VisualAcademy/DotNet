a14b6d86681cb3273b07b6a49ad4c1d614d12c19
RedPlus <redplus@redplus.net>
2020-12-23 04:44:22 +0900

    IfElsePseudo

---
+++ DotNet/DotNet/13_IfElse/IfElsePseudo.cs
@@ -0,0 +1,27 @@
+﻿//[?] if와 else로 두 수의 차이를 양의 정수로 구하기 
+using System;
+
+class IfElsePseudo
+{
+    static void Main()
+    {
+        int diff = 0; // 두 수의 차이
+
+        int first = 5;
+        int second = 3;
+
+        //int first = 3;
+        //int second = 5;
+
+        if (first > second)
+        {
+            diff = first - second; // first 변수가 더 큼
+        }
+        else
+        {
+            diff = second - first; // second 변수가 더 큼 
+        }
+
+        Console.WriteLine($"{first}와 {second}의 차이: {diff}");
+    }
+}
