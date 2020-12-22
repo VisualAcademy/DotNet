8c539fd5f16f37f77c28bb9bb97d9b9a13f58a8c
RedPlus <redplus@redplus.net>
2020-12-23 04:11:29 +0900

    ConstPseudo

---
+++ DotNet/DotNet/05_Variable/ConstPseudo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class ConstPseudo
+{
+    static void Main()
+    {
+        //[?] 상수(Constant): 변하지 않는 변수, 읽기 전용 변수
+        const int bestScore = 100;
+        //bestScore = 101;
+        Console.WriteLine(bestScore);
+    }
+}
