1787aa90d9255b7395f3b805825125968401ed38
RedPlus <redplus@redplus.net>
2020-12-23 00:24:00 +0900

    NumberPseudo

---
+++ DotNet/DotNet/06_Number/NumberPseudo.cs
@@ -0,0 +1,14 @@
+﻿// 숫자 데이터 형식: int, float 등의 키워드로 숫자 데이터 저장
+using System;
+
+class NumberPseudo
+{
+    static void Main()
+    {
+        int i = 1234; // 정수 형식
+        float f = 3.14f; // 실수 형식
+
+        Console.WriteLine(i);
+        Console.WriteLine(f);
+    }
+}
