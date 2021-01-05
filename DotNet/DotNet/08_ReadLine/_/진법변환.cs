624bb1f7853882fdc3b1ea4ff80eba9b0f57e3aa
RedPlus <redplus@redplus.net>
2021-01-05 14:07:13 +0900

    진법변환

---
+++ DotNet/DotNet/08_ReadLine/_/진법변환.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace 진법변환
+{
+    class 진법변환
+    {
+        static void Main(string[] args)
+        {
+            // 10진수 10을 2진수로 변경하면 1010
+            Console.WriteLine($"10 : {Convert.ToString(10, 2)}"); // 8421 => 1010
+
+            // 2진수 1010을 10진수로 변경하면 10
+            Console.WriteLine($"1010 : {Convert.ToInt32("1010", 2)}"); // 10
+        }
+    }
+}
