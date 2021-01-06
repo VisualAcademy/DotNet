61b478971c04609816e3a18638541521d803d92c
RedPlus <redplus@redplus.net>
2021-01-07 02:39:10 +0900

    VarPseudo

---
+++ DotNet/DotNet/08_ReadLine/VarPseudo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class VarPseudo
+{
+    static void Main()
+    {
+        var num = 1234;
+        //num = "1234"; 
+        num = 4321;
+        Console.WriteLine(num);
+    }
+}
