de10a73fc1625d64d75ba00a672469bef2e4e9d5
RedPlus <redplus@redplus.net>
2021-01-08 03:10:25 +0900

    IfElseScoreGradeOld

---
+++ DotNet/DotNet/13_IfElse/17_IfElseScoreGrade/IfElseScoreGradeOld.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class IfElseScoreGradeOld
+{
+    static void Main()
+    {
+        Console.Write("점수: ");
+        int score = Convert.ToInt32(Console.ReadLine());
+        string grade = "";
+
+        if (score >= 90)
+        {
+            grade = "금상";
+        }
+        else
+        {
+            if (score >= 80)
+            {
+                grade = "은상";
+            }
+            else
+            {
+                if (score >= 70)
+                {
+                    grade = "동상";
+                }
+                else
+                {
+                    grade = "장려상";
+                }
+            }
+        }
+
+        Console.WriteLine($"{grade}을 수상하였습니다.");
+    }
+}
