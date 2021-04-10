a876e2f773088993846b49a603f01fcd942d7af6
RedPlus <redplus@redplus.net>
2021-04-11 08:48:18 +0900

    Code Cleanup

--- DotNet/DotNet/17_BreakContinueGoto/05_ForIfContinue/ForIfContinue.cs
+++ DotNet/DotNet/17_BreakContinueGoto/05_ForIfContinue/ForIfContinue.cs
@@ -9,7 +9,7 @@ class ForIfContinue
         {
             if (i % 2 == 0)
             {
-                // 현재 반복 중지 후 다음 반복으로 이동
+                //[!] 현재 반복 중지 후 다음 반복으로 이동
                 continue; // 짝수 건너뛰기
             }
             Console.WriteLine(i); // 1, 3, 5
