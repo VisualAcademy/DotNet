341e23855c8b3b19a56eeeb0485f73dc7c4c07bf
RedPlus <redplus@redplus.net>
2021-05-20 05:39:52 +0900

    Code Cleanup

--- DotNet/DotNet/10_AssignmentIncrement/06_ShortcutOperatorDemo/ShortcutOperatorDemo.cs
+++ DotNet/DotNet/10_AssignmentIncrement/06_ShortcutOperatorDemo/ShortcutOperatorDemo.cs
@@ -1,4 +1,4 @@
-﻿// 할당(대입) 연산자: +=, -=, *=, /=, %=
+﻿//[?] 할당(대입) 연산자: =, +=, -=, *=, /=, %=
 using System;
 
 class ShortcutOperatorDemo
@@ -14,7 +14,7 @@ class ShortcutOperatorDemo
 
         // 현재 b는 8
 
-        b -= a; // 가감: b에서 a를 뺀 후 다시 b에 할당
+        b -= a; // 감산: b에서 a를 뺀 후 다시 b에 할당
         Console.WriteLine("{0}", b); // 5
     }
 }
