96b217228a742f8bf3e29a14a32ba3e236dda76f
RedPlus <redplus@redplus.net>
2021-06-02 06:35:51 +0900

    Code Cleanup

--- DotNet/DotNet/18_Array/06_ArrayOne/ArrayOne1.cs
+++ DotNet/DotNet/18_Array/06_ArrayOne/ArrayOne1.cs
@@ -12,13 +12,13 @@ class ArrayOne1
         intArray[1] = 2;
         intArray[2] = 3;
 
-        //[1] for문 사용 출력: 정확하게 배열의 범위를 알고 있을 때
+        //[1] for 문 사용 출력: 정확하게 배열의 범위를 알고 있을 때
         for (int i = 0; i < 3; i++) // 배열 참조
         {
             Console.WriteLine($"{i}번째 인덱스: {intArray[i]}");
         }
 
-        //[2] foreach문 사용 출력: intArray에 데이터가 있는 동안 반복
+        //[2] foreach 문 사용 출력: intArray에 데이터가 있는 동안 반복
         foreach (int intValue in intArray)
         {
             Console.WriteLine("{0}", intValue);
