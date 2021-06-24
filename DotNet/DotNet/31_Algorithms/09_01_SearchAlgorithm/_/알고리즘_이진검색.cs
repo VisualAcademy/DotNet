b8ff9efcfb349f251a2ff17022b5391fb8da4103
RedPlus <redplus@redplus.net>
2021-06-25 04:48:03 +0900

    Code Cleanup

---
+++ DotNet/DotNet/31_Algorithms/09_01_SearchAlgorithm/_/알고리즘_이진검색.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class 알고리즘_이진검색
+{
+    static void Main()
+    {
+        //[1] Input
+        int[] intNum = { 1, 2, 3, 4, 5, 6, 7 };
+        int key = 5;
+        int low, high, mid, flag;
+        //[2] Process
+        low = 0;
+        high = intNum.Length - 1;
+        flag = 0;
+        while (low <= high)
+        {
+            mid = (low + high) / 2;
+            //Console.WriteLine("비교");
+            if (key == intNum[mid])
+            {
+                flag = 1;
+                break;
+            }
+            if (key < intNum[mid])
+            {
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        //[3] Output
+        if (flag == 1)
+        {
+            Console.WriteLine("찾았습니다.");
+        }
+        else
+        {
+            Console.WriteLine("못찾았습니다.");
+        }
+    }
+}
---
+++ DotNet/DotNet/31_Algorithms/10_01_MergeAlgorithm/_/알고리즘_병합알고리즘.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class 알고리즘_병합알고리즘
+{
+    static void Main(string[] args)
+    {
+        //[1] Input
+        int[] a = { 1, 3, 5 };
+        int[] b = { 2, 4 };
+        int[] c = { 0, 0, 0, 0, 0 };
+        int i, j, k; i = j = k = 0;
+        //[2] Process : MERGE
+        while (i < 3 && j < 2)
+        {
+            if (a[i] <= b[j])
+            {
+                c[k++] = a[i++];
+            }
+            else
+            {
+                c[k++] = b[j++];
+            }
+        }
+        while (i < 3)
+        {
+            c[k++] = a[i++];
+        }
+        while (j < 2)
+        {
+            c[k++] = b[j++];
+        }
+        //[3] Output
+        for (int l = 0; l < 5; l++)
+        {
+            Console.WriteLine(c[l]);
+        }
+    }
+}
