69465c36845a5de386dde8ad570b243b1fb1937f
RedPlus <redplus@redplus.net>
2021-07-18 06:01:43 +0900

    Code Cleanup

---
+++ DotNet/DotNet/66_Etc/데이터구조/데이터구조_큐설명(닷넷).cs
@@ -0,0 +1,25 @@
+﻿/*
+큐(Queue) : FIFO(First In First Out) 형태의 데이터 저장
+- Enqueue() : 큐에 데이터 입력
+- Dequeue() : 큐에서 데이터 출력
+*/
+using System;
+using System.Collections;
+
+public class 큐설명
+{
+    static void Main(string[] args)
+    {
+        // 큐 생성
+        Queue q = new Queue();
+        // 큐에 데이터 입력 : Enqueue()
+        q.Enqueue("C언어");
+        q.Enqueue("C++");
+        q.Enqueue("C#");
+        // 큐에서 데이터 출력 : Dequeue()
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(q.Dequeue());
+        }
+    }
+}
