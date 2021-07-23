7d1b3f085c7642e91e89846d34d41f03dd903c7d
RedPlus <redplus@redplus.net>
2021-07-23 11:55:19 +0900

    해시테이블

--- DotNet/DotNet/17_BreakContinueGoto/03_BreakDemo/BreakDemo.cs
+++ DotNet/DotNet/17_BreakContinueGoto/03_BreakDemo/BreakDemo.cs
@@ -16,4 +16,3 @@ class BreakDemo
         Console.WriteLine();
     }
 }
-
---
+++ DotNet/DotNet/66_Etc/데이터구조/데이터구조_해시테이블(닷넷).cs
@@ -0,0 +1,28 @@
+﻿// 해시테이블(Hashtable) : 배열 -> 정수형 인덱스
+// a[3] = {1,2,3}; a[인덱스] : 정수형 인덱스
+// aa["첫번째"] = "값";
+using System;
+using System.Collections;
+
+public class 해시테이블
+{
+    public static void Main()
+    {
+        //[1] 배열
+        int[] a = { 1, 2, 3 };
+        for (int i = 0; i < 3; i++)
+        {
+            Console.WriteLine(a[i]);
+        }
+
+        //[2] 해시테이블
+        Hashtable h = new Hashtable();
+        h[1] = "닷넷코리아";
+        h["두번째"] = "제일스터디";
+        h[11.22] = "하와소";
+
+        Console.WriteLine(h[1]);
+        Console.WriteLine(h["두번째"]);
+        Console.WriteLine(h[11.22]);
+    }
+}
