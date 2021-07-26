90bc343a609b81a469e901daa06a10632cd8bab3
RedPlus <redplus@redplus.net>
2021-07-26 23:13:36 +0900

    Code Cleanup

---
+++ DotNet/DotNet/66_Etc/데이터구조/자료구조_스택(닷넷클래스사용).cs
@@ -0,0 +1,26 @@
+﻿/*
+ * .NET Class의 Stack 클래스 사용	 
+*/
+using System;
+using System.Collections;
+
+public class 스택
+{
+	public static void Main()
+	{
+		//[1] 스택 인스턴스 생성
+		Stack stack = new Stack();
+
+		//[2] Push
+		Console.WriteLine("Push : C언어");
+		stack.Push("C언어");
+		Console.WriteLine("Push : C#");
+		stack.Push("C#");
+
+		Console.WriteLine("===============");
+
+		//[3] Pop
+		Console.WriteLine("Pop : {0}", stack.Pop());
+		Console.WriteLine("Pop : {0}", stack.Pop());
+	}
+}
