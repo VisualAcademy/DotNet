1465ab26c131eba1bb5a1025e2dc38a53b71f644
RedPlus <redplus@redplus.net>
2021-09-06 15:48:45 +0900

    Code Cleanup

--- DotNet/DotNet/12_BitShiftConditional/01_BitAnd/BitAnd.cs
+++ DotNet/DotNet/12_BitShiftConditional/01_BitAnd/BitAnd.cs
@@ -5,18 +5,18 @@ class BitAnd
 {
     static void Main()
     {
-        byte x = 0b1010; // 10
-        byte y = 0b1100; // 12
+        byte x = 0b1010; // 10진수: 10
+        byte y = 0b1100; // 10진수: 12
 
         // x를 이진수로 표현 -> 십진수로 표현
         Console.WriteLine($"  {Convert.ToString(x, 2)} -> {x}");
 
         // y를 이진수로 표현 -> 십진수로 표현
-        Console.WriteLine($"& {Convert.ToString(y, 2)} -> {y}"); 
+        Console.WriteLine($"& {Convert.ToString(y, 2)} -> {y}");
 
         Console.WriteLine($"------");
 
         // x AND y를 이진수로 표현 -> 십진수로 2칸 잡고 표현 
-        Console.WriteLine($"  {Convert.ToString(x & y, 2)} -> {x & y, 2}");
+        Console.WriteLine($"  {Convert.ToString(x & y, 2)} -> {x & y,2}");
     }
 }
