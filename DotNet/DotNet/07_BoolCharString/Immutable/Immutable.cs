3315a836a0703c23f48d0219050c57523e4e800f
Park YongJune <redplus@redplus.net>
2021-10-13 18:13:07 +0900

    Code Cleanup

--- DotNet/DotNet/07_BoolCharString/Immutable/Immutable.cs
+++ DotNet/DotNet/07_BoolCharString/Immutable/Immutable.cs
@@ -5,11 +5,12 @@ class Immutable
     static void Main()
     {
         // 상수: 변하지 않는 값
-        const string name = "박용준";
+        const string name = "백두산";
 
         // 변수: 변하는 값
-        int age = 45;
+        int age = 102;
 
+        // 이름: 백두산, 나이: 102
         Console.WriteLine($"이름: {name}, 나이: {age}");
     }
 }
