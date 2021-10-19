acec4acc6601faebb32b0723cb07a479b9f35c28
Park YongJune <redplus@redplus.net>
2021-10-19 19:50:39 +0900

    code cleanup

--- DotNet/DotNet/13_IfElse/12_IfElse/IfElse.cs
+++ DotNet/DotNet/13_IfElse/12_IfElse/IfElse.cs
@@ -1,18 +1,19 @@
-﻿using System;
+﻿// 조건이 같은지 다른지 확인하기
+using System;
 
 class IfElse
 {
     static void Main()
     {
-        double pi = 3.14;
+        const double PI = 3.14;
 
-        if (pi == 3.14)
+        if (PI == 3.14)
         {
-            Console.WriteLine("pi는 3.14입니다.");        // 참일 때 실행
+            Console.WriteLine("PI는 3.14입니다.");        // 참일 때 실행
         }
         else
         {
-            Console.WriteLine("pi는 3.14가 아닙니다.");   // 거짓일 때 실행
+            Console.WriteLine("PI는 3.14가 아닙니다.");   // 거짓일 때 실행
         }
     }
 }
