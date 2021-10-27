bf9ae329713602cb8133e4cede8822415f063b3b
Park YongJune <redplus@redplus.net>
2021-10-27 11:08:44 +0900

    1부터 100까지 정수의 합계 구하기

--- DotNet/DotNet/15_For/05_ForSumEven/ForSumEven.cs
+++ DotNet/DotNet/15_For/05_ForSumEven/ForSumEven.cs
@@ -1,4 +1,4 @@
-﻿// 1부터 n까지 정수 중 짝수의 합을 구하는 프로그램
+﻿//[?] 1부터 n까지 정수 중 짝수의 합을 구하는 프로그램
 using System;
 
 class ForSumEven
@@ -8,14 +8,15 @@ class ForSumEven
         int n = 5;
         int sum = 0;
 
-        for (int i = 1; i <= n; i++)    // n == 5이므로 i가 1, 2, 3, 4일 때
+        for (int i = 1; i <= n; i++) // n == 5이므로 i가 1, 2, 3, 4일 때
         {
-            if (i % 2 == 0)
+            if (i % 2 == 0) // 모든 숫자를 2로 나누었을 때 0과 같으면 짝수 
             {
-                sum += i;               // 짝수 2, 4 더하기 
+                sum += i; // 짝수만(2, 4) 더하기 
             }
         }
 
+        // 1부터 5까지 짝수의 합: 6
         Console.WriteLine($"1부터 {n}까지 짝수의 합: {sum}");
     }
 }
--- DotNet/DotNet/15_For/06_SeeSharpSum100/SeeSharpSum100.cs
+++ DotNet/DotNet/15_For/06_SeeSharpSum100/SeeSharpSum100.cs
@@ -1,19 +1,20 @@
-﻿using System;
+﻿//[?] 1부터 100까지 정수의 합계 구하기
+using System;
 
 class SeeSharpSum100
 {
     static void Main()
     {
-        // Input
+        //[1] Input(입력)
         int sum = 0;
 
-        // Process
+        //[2] Process(처리)
         for (int i = 1; i <= 100; i++) // i가 1에서 100까지 100번 반복
         {
-            sum += i; // sum = sum + i; // 누적
+            sum += i; // sum = sum + i; // 누적(SUM)
         }
 
-        // Output
-        Console.WriteLine("1부터 100까지 정수의 합: {0}",  sum);
+        //[3] Output(출력)
+        Console.WriteLine("1부터 100까지 정수의 합: {0}",  sum); // 5050
     }
 }
