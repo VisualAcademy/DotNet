43eb81586ca031bf2ba8557579f51e604c75623e
Park YongJune <redplus@redplus.net>
2021-10-27 10:09:50 +0900

    Code Cleanup

--- DotNet/DotNet/15_For/01_ForDescription/ForDescription.cs
+++ DotNet/DotNet/15_For/01_ForDescription/ForDescription.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿//[?] for 문을 사용하여 "안녕하세요." 3번 출력하기
+using System;
 
 class ForDescription
 {
     static void Main()
     {
-        // for (초기식;조건식;증감식) { }
+        //[!] for (초기식; 조건식; 증감식) { 실행문; }
         for (int i = 0; i < 3; i++) // i가 0, 1, 2일 때 
         {
             Console.WriteLine("안녕하세요."); // 안녕하세요. 출력
