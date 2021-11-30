6e27832d0c44eaefbb41c1e3d5905ff6b0eacea0
Park YongJune <redplus@redplus.net>
2021-11-30 12:15:21 +0900

    각 점수를 4칸 잡고 오른쪽 정렬해서 출력

--- DotNet/DotNet/18_Array/22_ArraySumAverage/ArraySumAverage.cs
+++ DotNet/DotNet/18_Array/22_ArraySumAverage/ArraySumAverage.cs
@@ -13,8 +13,8 @@ class ArraySumAverage
 
         for (int i = 0; i < 3; i++)
         {
-            scores[i, 2] = scores[i, 0] + scores[i, 1]; // SUM
-            scores[i, 3] = scores[i, 2] / 2; // AVERAGE
+            scores[i, 2] = scores[i, 0] + scores[i, 1]; // SUM(합계)
+            scores[i, 3] = scores[i, 2] / 2; // AVERAGE(평균)
         }
 
         Console.WriteLine("국어 영어 합계 평균");
@@ -22,6 +22,7 @@ class ArraySumAverage
         {
             for (int j = 0; j < 4; j++)
             {
+                // 각 점수를 4칸 잡고 오른쪽 정렬해서 출력 
                 Console.Write($"{scores[i, j],4} ");
             }
             Console.WriteLine();
