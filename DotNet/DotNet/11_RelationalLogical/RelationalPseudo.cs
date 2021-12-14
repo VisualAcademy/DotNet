408a2b120425ff5293421a9f67d093cfe607f2a0
Park YongJune <redplus@redplus.net>
2021-12-14 12:16:27 +0900

    RelationalPseudo

---
+++ DotNet/DotNet/11_RelationalLogical/RelationalPseudo.cs
@@ -0,0 +1,20 @@
+﻿// RelationalPseudo.*
+// 관계형 연산자(비교 연산자;Relational Operator, Comparative Operator)
+// 관계형 연산자: < 작음, <= 작거나 같음, > 큼, >= 크거나 같음, == 같음, != 다름
+using System;
+
+class RelationalPseudo
+{
+    static void Main()
+    {
+        int x = 3;
+        int y = 5;
+
+        Console.WriteLine(x == y);  // 3 == 5 -> false
+        Console.WriteLine(x != y);  // 3 != 5 -> true
+        Console.WriteLine(x > y);   // 3 > 5 -> false
+        Console.WriteLine(x >= y);  // 3 >= 5 -> false
+        Console.WriteLine(x < y);   // 3 < 5 -> true
+        Console.WriteLine(x <= y);  // 3 <= 5 -> true
+    }
+}
