ccabefdce79cefc99a80e727f491635e5898e249
Park YongJune <redplus@redplus.net>
2022-03-01 16:38:01 +0900

    VerbatimStringLiterals

---
+++ DotNet/DotNet/07_BoolCharString/VerbatimStringLiterals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class VerbatimStringLiterals
+{
+    static void Main()
+    {
+        Console.WriteLine("C:\\Users\\VisualAcademy"); // "C:\\Users\\VisualAcademy" - C:\Users\VisualAcademy
+        Console.WriteLine(@"C:\Users\VisualAcademy"); // "C:\\Users\\VisualAcademy" - C:\Users\VisualAcademy
+        Console.WriteLine(@""""); // "\"" - "
+        Console.WriteLine(@"""VisualAcademy"""); // "\"VisualAcademy\"" - "VisualAcademy"
+    }
+}
