f60917b7805409a34aacb321b997d7f6f0354eff
Park YongJune <redplus@redplus.net>
2022-03-05 23:14:52 +0900

    .

--- DotNet/DotNet/56_Async/AsyncAwaitDescription/AsyncAwaitDescription.cs
+++ DotNet/DotNet/56_Async/AsyncAwaitDescription/AsyncAwaitDescription.cs
@@ -8,7 +8,7 @@ class AsyncAwaitDescription
     {
         Task.Run(() => DoPrint()); // 1, 2, 3, ...
         Console.WriteLine("[?] async await 사용 예제");
-        Thread.Sleep(1);
+        Thread.Sleep(1); // 1 밀리초 대기 
         //Console.ReadLine();
     } // end Main
 
@@ -21,6 +21,7 @@ class AsyncAwaitDescription
     {
         await Task.Run(() =>
         {
+            //[!] 오래 걸리는 작업 들어오는 곳
             for (int i = 0; i < 300; i++)
             {
                 Console.WriteLine(i + 1);
