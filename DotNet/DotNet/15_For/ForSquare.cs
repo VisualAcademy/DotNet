e33faf073a62a66e984d5951cbb70a37b4772e4d
Park YongJune <redplus@redplus.net>
2022-06-08 06:52:04 +0900

    ForSquare

---
+++ DotNet/DotNet/15_For/ForSquare.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class ForSquare
+{
+    static void Main()
+    {
+        // 바깥쪽 for 문: 행 반복
+        for (int i = 1; i <= 3; i++)
+        {
+            // 안쪽 for 문: 열 반복
+            for (int j = 1; j <= 3; j++)
+            {
+                Console.Write("*"); // 별 모양 출력 
+            }
+            Console.WriteLine(); // 한 행 반복 후 줄바꿈 
+        }
+    }
+}
