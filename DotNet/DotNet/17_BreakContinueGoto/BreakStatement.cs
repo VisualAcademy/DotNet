592023c0b431eee674b91799cd737c9ba7a425d1
Park YongJune <redplus@redplus.net>
2022-08-28 08:57:28 +0900

    .

---
+++ DotNet/DotNet/17_BreakContinueGoto/BreakStatement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class BreakStatement
+{
+    static void Main()
+    {
+        for (int i = 1; i <= 10; i++) // 1부터 10까지 반복
+        {
+            Console.WriteLine(i); // 인덱스 변수 i 출력
+
+            if (i == 3)
+            {
+                break; // 현재 코드를 만나면 현재 for 문을 종료함
+            }
+        }
+    }
+}
