ef734e72bdc626aadd29e8872567b0cf04dbd215
Park YongJune <redplus@redplus.net>
2022-09-15 07:55:03 +0900

    .

---
+++ DotNet/DotNet/15_For/ForNestedPseudo.cs
@@ -0,0 +1,20 @@
+﻿class ForNestedPseudo
+{
+    static void Main()
+    {
+
+
+
+        for (int i = 0; i < 5; i++)
+        {
+            for (int j = 0; j < 5; j++)
+            {
+                // 가로 처리
+            }
+            // 세로 처리
+        }
+
+
+
+    }
+}
