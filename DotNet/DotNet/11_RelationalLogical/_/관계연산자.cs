f9c0c16805d496f841c9d8959fe8b0b2bde676d6
Park YongJune <redplus@redplus.net>
2023-03-05 08:15:16 +0900

    관계연산자.cs

---
+++ DotNet/DotNet/11_RelationalLogical/_/관계연산자.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int first_num = 3, second_num = 5, greater, smaller;
+        greater = first_num > second_num ? 1 : 0;
+        smaller = first_num < second_num ? 1 : 0;
+        Console.WriteLine("first_num = {0} second_num = {1} " +
+                          "first_num > second_num = {2} " +
+                          "first_num < second_num = {3}",
+                          first_num, second_num, greater, smaller);
+    }
+}
