f049fe5393f0e15f18a76cd91a9fe0a9d0f5d705
Park YongJune <redplus@redplus.net>
2023-03-06 15:59:06 +0900

    .

---
+++ DotNet/DotNetEightTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class ReverseTriangle
+{
+    static void Main(string[] args)
+    {
+        for (int i = 10; i >= 0; i--)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
