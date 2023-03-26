7f536cd14859bbb6f4c1e72cfd0ec88cfc4e2559
Park YongJune <redplus@redplus.net>
2023-03-26 15:16:31 +0900

    여러 개의 변수 선언 및 초기화하기

---
+++ DotNet/DotNet/05_Variable/.Dojang/PracticeVariable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class PracticeVariable
+{
+    static void Main()
+    {
+        int num1 = 15, num2 = 25, num3 = 35;
+
+        Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+    }
+}
