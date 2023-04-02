cc41454368b676590ab5d1557fb3f85b7b4241b3
Park YongJune <redplus@redplus.net>
2023-04-03 04:36:55 +0900

    덧셈, 뺄셈, 나눗셈

--- DotNet/DotNet/09_Operator/12_OperatorArithmetic/OperatorArithmetic.cs
+++ DotNet/DotNet/09_Operator/12_OperatorArithmetic/OperatorArithmetic.cs
@@ -9,9 +9,9 @@ class OperatorArithmetic
         int a = 5;
         int b = 3;
 
-        Console.WriteLine(a + b); // 더하기: 8
-        Console.WriteLine(a - b); // 빼기: 2
-        Console.WriteLine(a * b); // 곱하기: 15
+        Console.WriteLine(a + b); // 덧셈: 8
+        Console.WriteLine(a - b); // 뺄셈: 2
+        Console.WriteLine(a * b); // 곱셈: 15
         Console.WriteLine(a / b); // 몫: 1  
         Console.WriteLine(
             "{0} % {1} = {2}", a, b, (a % b)); // 나머지: 2
