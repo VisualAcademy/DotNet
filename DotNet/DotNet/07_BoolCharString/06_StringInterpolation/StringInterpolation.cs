bbd78e06abff7f749e732f08c772903fdbb6e7b6
Park YongJune <redplus@redplus.net>
2023-04-03 04:34:36 +0900

    .

--- DotNet/DotNet/07_BoolCharString/06_StringInterpolation/StringInterpolation.cs
+++ DotNet/DotNet/07_BoolCharString/06_StringInterpolation/StringInterpolation.cs
@@ -7,7 +7,7 @@ class StringInterpolation
         // 문자열 보간(String Interpolation) 
         string message = "String Interpolation";
         Console.WriteLine("Message: {0}", message); // WriteLine() 메서드 기본 제공
-        Console.WriteLine("Message: " + message); // 더하기 연산자
+        Console.WriteLine("Message: " + message); // 덧셈 연산자
         Console.WriteLine($"Message: {message}"); // 문자열 보간법
     }
 }
