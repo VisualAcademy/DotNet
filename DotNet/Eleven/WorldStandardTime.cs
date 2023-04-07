5bd4f50cb45e9fa19ca0b200c47cc8f325c68dbb
Park YongJune <redplus@redplus.net>
2023-04-08 05:03:14 +0900

    .

---
+++ DotNet/Eleven/WorldStandardTime.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WorldStandardTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 현재 시간을 UTC로 가져옵니다.
+            DateTime utcNow = DateTime.UtcNow;
+
+            // 세계 표준시 출력
+            Console.WriteLine("세계 표준시 : {0}.{1}.{2} {3}:{4}:{5}",
+                utcNow.Year,
+                utcNow.Month,
+                utcNow.Day,
+                utcNow.Hour,
+                utcNow.Minute,
+                utcNow.Second
+            );
+        }
+    }
+}
