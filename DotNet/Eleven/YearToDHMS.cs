807c23aa1495da7099097ad7db880b2ff6efc8ba
Park YongJune <redplus@redplus.net>
2023-04-10 11:47:16 +0900

    .

---
+++ DotNet/Eleven/YearToDHMS.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace YearToDHMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day, hour, minute, second;
+            double year = 365.2425;
+            double dHour;
+            double dMinute;
+
+            day = (int)year; // 일
+
+            dHour = (year - day) * 24;
+            hour = (int)dHour; // 시
+
+            dMinute = (dHour - hour) * 60;
+            minute = (int)dMinute; // 분
+
+            second = (int)((dMinute - minute) * 60); // 초
+
+            Console.WriteLine($"1년 = {day}일 {hour}시간 {minute}분 {second}초");
+        }
+    }
+}
