522c606261f2625f24bdd2eeb0a352e22d6d9265
Park YongJune <redplus@redplus.net>
2023-04-10 16:00:04 +0900

    .

---
+++ DotNet/DotNet/31_Algorithms/09_01_SearchAlgorithm/SequentialSearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class SequentialSearchExample
+{
+    static void Main()
+    {
+        int i;
+        int[] data = { 1, 3, 5, 7, 9, 11 };
+        int search = 0;
+        int n = data.Length;
+        bool flag = false;
+
+        Console.Write("검색할 정수(1~20): ");
+        search = int.Parse(Console.ReadLine());
+
+        for (i = 0; i < n; i++)
+        {
+            if (search == data[i])
+            {
+                flag = true;
+            }
+        }
+
+        if (flag == true)
+        {
+            Console.WriteLine("{0}을(를) 찾았습니다.", search);
+        }
+        else
+        {
+            Console.WriteLine("{0}을(를) 못 찾았습니다.", search);
+        }
+    }
+}
