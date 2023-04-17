81c8a6d8fa8b0ab81d35f9684ce59f4747a0f0e7
Park YongJune <redplus@redplus.net>
2023-04-18 02:27:46 +0900

    .

---
+++ DotNet/DotNet/06_Number/NumberLiteral.cs
@@ -0,0 +1,16 @@
+﻿// 정수 리터럴
+using System;
+
+class NumberLiteral
+{
+    static void Main()
+    {
+        Console.WriteLine("{0}", 1234); // 10진수
+        Console.WriteLine("{0}", 0x1234); // 16진수
+
+        int octalValue = Convert.ToInt32("1234", 8); // 8진수를 10진수로 변환
+        Console.WriteLine("{0}", octalValue);
+
+        Console.ReadLine();
+    }
+}
