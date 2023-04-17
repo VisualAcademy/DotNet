c363d60afc6ba879738223b4639f7f68480cc903
Park YongJune <redplus@redplus.net>
2023-04-18 04:53:42 +0900

    .

---
+++ DotNet/DotNet/06_Number/NumberDataTypesPractice.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class NumberDataTypesPractice
+{
+    static void Main()
+    {
+        int number1 = 42;
+        float number2 = 3.14f;
+        decimal number3 = 9876.5432m; // (   ) number3 = 9876.5432m;
+
+        Console.WriteLine("number1: " + number1);
+        Console.WriteLine("number2: " + number2);
+        Console.WriteLine("number3: " + number3);
+    }
+}
---
+++ DotNet/Gilbut/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Gilbut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
