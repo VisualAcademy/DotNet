faec8936ccc673e8f981929696d7d0d9a25f4295
Park YongJune <redplus@redplus.net>
2023-04-22 02:48:29 +0900

    .

---
+++ DotNet/Dojang/SumOfTwoIntegers.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class SumOfTwoIntegers
+{
+    static void Main()
+    {
+        int a, b;
+        Console.WriteLine("두 정수를 입력하세요: ");
+        a = Convert.ToInt32(Console.ReadLine());
+        b = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("합: " + (a + b));
+    }
+}
