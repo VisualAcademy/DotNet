86309ecb827942784f64e9908409b8688eb74e45
Park YongJune <redplus@redplus.net>
2023-04-25 11:48:14 +0900

    .

---
+++ DotNet/Dojang/GreatestCommonDivisorCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class GreatestCommonDivisorCalculator
+{
+    // 두 정수 a와 b의 최대 공약수를 구하는 메서드
+    static int Gcd(int a, int b)
+    {
+        int c;
+        // b가 0이 아닐 때까지 반복
+        while (b != 0)
+        {
+            // a를 b로 나눈 나머지를 c에 저장
+            c = a % b;
+            // a에는 b 값을 대입하여 b가 더 작은 값이 되도록 함
+            a = b;
+            // b에는 c 값을 대입하여 b를 a를 나눈 나머지로 변경함
+            b = c;
+        }
+        // 최대 공약수를 반환
+        return a;
+    }
+
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("두 정수를 입력하세요: ");
+        // 사용자로부터 두 정수를 입력받음
+        string[] input = Console.ReadLine().Split();
+        a = int.Parse(input[0]);
+        b = int.Parse(input[1]);
+
+        // 최대 공약수를 계산하고 출력
+        Console.WriteLine("{0}와 {1}의 최대 공약수는: {2}입니다.", a, b, Gcd(a, b));
+    }
+}
---
+++ DotNet/Dojang/LeastCommonMultipleCalculator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+class LeastCommonMultipleCalculator
+{
+    // 두 정수 a와 b의 최대 공약수를 구하는 메서드(재귀 메서드 사용) 
+    static int Gcd(int a, int b)
+    {
+        // b가 0이면 a가 최대 공약수이므로 반환
+        if (b == 0)
+        {
+            return a;
+        }
+        // b가 0이 아닌 경우, 재귀적으로 최대 공약수를 찾음
+        return Gcd(b, a % b);
+    }
+
+    // 두 정수 a와 b의 최소 공배수를 구하는 메서드
+    static int Lcm(int a, int b)
+    {
+        // 최소 공배수 공식: a * b / gcd(a, b)
+        return a * b / Gcd(a, b);
+    }
+
+    static void Main()
+    {
+        int a, b;
+
+        Console.Write("두 숫자를 입력하세요: ");
+        string input = Console.ReadLine();
+        string[] inputs = input.Split();
+        a = int.Parse(inputs[0]);
+        b = int.Parse(inputs[1]);
+
+        // 최소 공배수를 계산하고 출력하는 부분
+        Console.WriteLine("{0}와 {1}의 최소공배수는 {2}입니다.", a, b, Lcm(a, b));
+    }
+}
