36d4c25b383aa01dd1899ecf3f8f7ccb4d1913a6
Park YongJune <redplus@redplus.net>
2023-04-29 22:57:44 +0900

    .

---
+++ DotNet/DotNet/58_IO/FileReadString/FileReadString.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+class FileReadString
+{
+    static void Main()
+    {
+        try
+        {
+            using (StreamReader sr = new StreamReader("example.txt"))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("파일을 열 수 없습니다.");
+            Console.WriteLine(e.Message);
+        }
+    }
+}
