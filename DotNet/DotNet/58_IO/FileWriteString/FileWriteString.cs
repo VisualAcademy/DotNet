19623e94019b690fa0c9c5f583c72158e7dc7e95
Park YongJune <redplus@redplus.net>
2023-04-30 13:44:42 +0900

    .

---
+++ DotNet/DotNet/58_IO/FileWriteString/FileWriteString.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+class FileWriteString
+{
+    static void Main()
+    {
+        Console.Write("저장할 문자열을 입력하세요: ");
+        string str = Console.ReadLine();
+
+        try
+        {
+            using (StreamWriter file = new StreamWriter("example.txt"))
+            {
+                file.Write(str);
+            }
+            Console.WriteLine("파일에 저장되었습니다.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("파일을 열 수 없습니다. 에러: " + ex.Message);
+        }
+    }
+}
