2f0693973fcf396024fbde7f2a5697fa2abee53d
Park YongJune <redplus@redplus.net>
2023-05-14 23:41:11 +0900

    .

---
+++ DotNet/Dojang/StringCopy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class StringCopy
+{
+    static void Main()
+    {
+        Console.Write("원본 문자열을 입력하세요: ");
+        string src = Console.ReadLine();
+
+        string dest = String.Copy(src);
+
+        Console.WriteLine("복사된 문자열: " + dest);
+    }
+}
