41a3f1b3934ac9731547cdf06343f1e133c54ee7
Park YongJune <redplus@redplus.net>
2023-05-28 21:02:11 +0900

    .

---
+++ DotNet/DotNet/06_Number/04_DoubleDemo/DoubleDescription.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class DoubleDescription
+{
+    static void Main()
+    {
+        // 'd' 접미사 없음
+        double PI = 3.141592;
+        Console.WriteLine("'d' 접미사 없는 PI의 값: " + PI);
+
+        // 'd' 접미사 포함
+        double PIWithSuffix = 3.141592d;
+        Console.WriteLine("'d' 접미사가 있는 PI의 값: " + PIWithSuffix);
+    }
+}
