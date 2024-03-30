285a3eb9057a0065a8db9cd027598aae304e2fa2
Park YongJune <redplus@redplus.net>
2024-03-31 01:22:20 +0900

    정보처리산업기사 실기 시험 기출 문제 - 비트 및 논리 연산 출력

---
+++ DotNet/DotNet/12_BitShiftConditional/BitwiseLogicalOperations/BitwiseLogicalOperations.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class BitwiseLogicalOperations
+{
+    static void Main()
+    {
+        int x = 1;
+        Console.WriteLine(!(x > 0));
+        Console.WriteLine(x > 0 || x < 4);
+        Console.WriteLine(x << 2);
+        Console.WriteLine(x & 2);
+        Console.WriteLine(x % 3);
+    }
+}
