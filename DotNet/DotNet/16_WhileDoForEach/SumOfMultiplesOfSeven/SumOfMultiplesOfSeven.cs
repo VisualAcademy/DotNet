4ba0fa6f1ef199f8c5e4e2927f64ef5c0683536c
Park YongJune <redplus@redplus.net>
2024-04-01 06:30:46 +0900

    정보처리기사 실기 시험 기출 문제 - 7의 배수 합계 계산

---
+++ DotNet/DotNet/16_WhileDoForEach/SumOfMultiplesOfSeven/SumOfMultiplesOfSeven.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class SumOfMultiplesOfSeven
+{
+    static void Main()
+    {
+        int i = 1, sum = 0;
+
+        while (i <= 50)
+        {
+            if (i % 7 == 0)
+            {
+                sum += i;
+            }
+            i++;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
