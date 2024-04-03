6c407132c784b51d8fe3bcd54f39657ecc6d300d
Park YongJune <redplus@redplus.net>
2024-04-03 21:53:44 +0900

    정보처리기사 실기 시험 기출 문제 - 클래스 상속과 메서드 호출

---
+++ DotNet/DotNet/43_Inheritance/ClassInheritanceDemo/ClassInheritanceDemo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DotNetInheritance
+{
+    class SuperClass
+    {
+        int num;
+        public SuperClass(int num) { this.num = num; }
+        public void Display() { Console.WriteLine($"num={num}"); }
+    }
+
+    class SubClass : SuperClass
+    {
+        public SubClass(int num) : base(num)
+        {
+            base.Display();
+        }
+    }
+
+    class ClassInheritanceDemo
+    {
+        static void Main()
+        {
+            SubClass obj = new SubClass(10);
+        }
+    }
+}
