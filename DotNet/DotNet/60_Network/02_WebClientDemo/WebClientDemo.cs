869e18c94d4c2bd9f68facf28df97e9258e42dc2
Park YongJune <redplus@redplus.net>
2024-04-14 20:43:45 +0900

    Code Cleanup

--- DotNet/DotNet/60_Network/02_WebClientDemo/WebClientDemo.cs
+++ DotNet/DotNet/60_Network/02_WebClientDemo/WebClientDemo.cs
@@ -8,22 +8,24 @@ class WebClientDemo
     {
         WebClient client = new WebClient();
 
-        // 동기적으로 출력
+        // 구글 웹 페이지의 내용을 동기적으로 다운로드하고 처음 10자만 출력
         string google = client.DownloadString("http://www.google.co.kr");
-        Console.WriteLine("Google: " + google.Substring(0, 10)); // 20자만 가져오기
+        Console.WriteLine("Google: " + google.Substring(0, 10)); // 처음 10자만 출력
 
+        // 네이버 웹 페이지의 내용을 동기적으로 다운로드하고 처음 10자만 출력
         string naver = client.DownloadString(new Uri("http://www.naver.com"));
         Console.WriteLine("Naver: " + naver.Substring(0, 10));
 
-        // 비동기적으로 출력
+        // 비동기적으로 웹 페이지 다운로드 시작
         client.DownloadStringAsync(new Uri("http://www.dotnetkorea.com"));
         client.DownloadStringCompleted += Client_DownloadStringCompleted;
-        Thread.Sleep(3000); // 잠시 대기
+        Thread.Sleep(3000); // 메인 스레드를 3초간 대기시킴
     }
 
     private static void Client_DownloadStringCompleted(object sender,
         DownloadStringCompletedEventArgs e)
     {
+        // 다운로드 완료 후 결과에서 줄바꿈을 제거하고 처음 10자만 출력
         string r = e.Result.Replace("\n", "").Substring(0, 10);
         Console.WriteLine($"DotNetKorea: {r}");
     }
