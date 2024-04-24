3e23a7098b9b38fdbad7c4eafcbf285cf8b6515c
Park YongJune <redplus@redplus.net>
2024-04-24 16:02:01 +0900

    .

---
+++ DotNet/DotNet/09_Operator/PrecisionDemo/PrecisionDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class PrecisionDemo
+{
+    static void Main()
+    {
+        int dividend = 3, divisor = 4;
+        double quotient;
+        quotient = (double)dividend / divisor;  // 개선된 부분
+        Console.WriteLine("Division result: {0}", quotient);
+    }
+}
