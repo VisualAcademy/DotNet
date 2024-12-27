8b38aa0c73f35a07b81031d744fee9ae72e8eae7
Park YongJune <redplus@redplus.net>
2024-12-27 22:39:01 +0900

    Boxing.cs

--- DotNet/DotNet/24_Class/BoxingUnboxing/Boxing.cs
+++ DotNet/DotNet/24_Class/BoxingUnboxing/Boxing.cs
@@ -4,6 +4,8 @@ class Boxing
 {
     static void Main()
     {
-        
+        int i = 1234;           // 값 형식(정수형) 선언
+        object o = i;           // 박싱: 값 형식을 참조 형식으로 변환
+        Console.WriteLine(o);   // 1234
     }
 }
