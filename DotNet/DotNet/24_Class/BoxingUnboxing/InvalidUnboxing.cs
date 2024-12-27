d9d8c7e85aa318063f2aa4430bf003f63d32161f
Park YongJune <redplus@redplus.net>
2024-12-27 23:19:27 +0900

    .

--- DotNet/DotNet/24_Class/BoxingUnboxing/InvalidUnboxing.cs
+++ DotNet/DotNet/24_Class/BoxingUnboxing/InvalidUnboxing.cs
@@ -1,12 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DotNet._24_Class.BoxingUnboxing
+class InvalidUnboxing
 {
-    internal class InvalidUnboxing
+    static void Main()
     {
+        
     }
 }
