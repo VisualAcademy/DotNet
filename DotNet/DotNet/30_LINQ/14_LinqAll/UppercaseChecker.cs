fb7a34224a2dccbe72fce0d1d1c7e0c67fd9e720
Park YongJune <redplus@redplus.net>
2025-01-02 07:04:16 +0900

    UppercaseChecker - 모든 문자가 대문자인지 확인

---
+++ DotNet/DotNet/30_LINQ/14_LinqAll/UppercaseChecker.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+class UppercaseChecker
+{
+    static void Main()
+    {
+        string input = "HELLO";
+
+        // 모든 문자가 대문자인지 확인
+        bool allUppercase = input.All(char.IsUpper);
+
+        if (allUppercase)
+        {
+            Console.WriteLine("모든 문자가 대문자입니다.");
+        }
+        else
+        {
+            Console.WriteLine("대문자가 아닌 문자가 포함되어 있습니다.");
+        }
+    }
+}
