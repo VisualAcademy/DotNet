03b2d97a3493c5c21d31b00a13aff38af0fab35b
Park YongJune <redplus@redplus.net>
2025-01-02 20:40:10 +0900

    StringAndCharArrayConverter - 문자 배열을 문자열로 변환하기

---
+++ DotNet/DotNet/25_String/StringAndCharArrayConverter/StringAndCharArrayConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class StringAndCharArrayConverter
+{
+    static void Main()
+    {
+        // 문자열 선언
+        string originalString = "Hello";
+
+        // 문자열을 문자 배열로 변환
+        char[] charArray = originalString.ToCharArray();
+        Console.WriteLine("문자 배열: " + string.Join(", ", charArray));
+
+        // 문자 배열을 다시 문자열로 변환
+        string newString = new string(charArray);
+        Console.WriteLine("다시 변환된 문자열: " + newString);
+    }
+}
