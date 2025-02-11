5bc9ba442d24c3a1352258fbb779dbae2a434ce9
Park YongJune <redplus@redplus.net>
2025-02-11 11:15:18 +0900

    .

--- DotNet/DotNet/23_Enum/01_ConsoleColorDemo/ConsoleColorDemo.cs
+++ DotNet/DotNet/23_Enum/01_ConsoleColorDemo/ConsoleColorDemo.cs
@@ -5,12 +5,12 @@ class ConsoleColorDemo
 {
     static void Main()
     {
-        // 전경색
+        // 전경색 변경
         Console.ForegroundColor = ConsoleColor.Blue;
         Console.WriteLine("Blue");
         Console.ResetColor();
 
-        // 배경색
+        // 배경색 변경
         Console.BackgroundColor = ConsoleColor.Yellow;
         Console.ForegroundColor = ConsoleColor.Red;
         Console.WriteLine("Red");
