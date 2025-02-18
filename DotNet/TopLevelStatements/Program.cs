a8a498d95300f4253e8be8cf5bc6d0709de522af
Park YongJune <redplus@redplus.net>
2025-02-18 19:40:42 +0900

    TLS

--- DotNet/TopLevelStatements/Program.cs
+++ DotNet/TopLevelStatements/Program.cs
@@ -1,2 +1,12 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
+//// Top-Level Statement
+//Console.WriteLine("Hello, World!");
