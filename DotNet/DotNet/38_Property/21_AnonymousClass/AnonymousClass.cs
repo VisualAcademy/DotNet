ef52fa96866b30d33fa858b1412432d70e6d1ba9
Park YongJune <redplus@redplus.net>
2025-05-01 08:53:58 +0900

    .

--- DotNet/DotNet/38_Property/21_AnonymousClass/AnonymousClass.cs
+++ DotNet/DotNet/38_Property/21_AnonymousClass/AnonymousClass.cs
@@ -6,7 +6,7 @@ class AnonymousClass
     static void Main()
     {
         // 익명 형식으로 개체를 생성하고 3개의 속성을 초기화
-        var presenter = new { Name = "박용준", Age = 45, Topic = "C#" };
+        var presenter = new { Name = "박용준", Age = 50, Topic = "C#" };
 
         // 개체 출력
         Console.WriteLine(
