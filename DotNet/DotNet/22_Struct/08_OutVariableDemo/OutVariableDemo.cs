937278acfd7834c732987fe454c5eaf42ffc3cfd
Park YongJune <redplus@redplus.net>
2025-05-07 00:39:40 +0900

    2025

--- DotNet/DotNet/22_Struct/08_OutVariableDemo/OutVariableDemo.cs
+++ DotNet/DotNet/22_Struct/08_OutVariableDemo/OutVariableDemo.cs
@@ -5,13 +5,13 @@ class OutVariableDemo
     static void Main()
     {
         // xmax 변수를 외부에서 선언할 필요없이 TryParse() 구문에 포함해서 사용
-        if (DateTime.TryParse("2020/12/25", out var xmas))
+        if (DateTime.TryParse("2025/12/25", out var xmas))
         {
             Console.WriteLine(xmas);
         }
 
         // "out var 변수" 구문은 실제로는 "out 데이터형식 변수" 형태로 새로운 변수를 생성
-        if (!DateTime.TryParse("20200707", out DateTime doubleSeven))
+        if (!DateTime.TryParse("20250707", out DateTime doubleSeven))
         {
             doubleSeven = DateTime.Now; // 만약, 변경이 불가능하다면 현재 시간으로 초기화
         }
