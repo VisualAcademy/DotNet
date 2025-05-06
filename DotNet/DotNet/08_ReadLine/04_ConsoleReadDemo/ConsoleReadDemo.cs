85101756a2a56f38e715d50367e90c358ca0d63b
Park YongJune <redplus@redplus.net>
2025-05-07 02:56:28 +0900

    .

--- DotNet/DotNet/08_ReadLine/04_ConsoleReadDemo/ConsoleReadDemo.cs
+++ DotNet/DotNet/08_ReadLine/04_ConsoleReadDemo/ConsoleReadDemo.cs
@@ -10,7 +10,7 @@ class ConsoleReadDemo
         //[2] 대문자 'A'가 입력되면 'A'에 해당하는 정수 값인 65가 출력됩니다.
         Console.WriteLine(x);
 
-        //[3] x가 65면 정수 65에 해당하는 유니코드 문자인 A가 출력됩니다. 
+        //[3] x가 65면 정수 65에 해당하는 문자인 A가 출력됩니다. 
         Console.WriteLine(Convert.ToChar(x)); 
     }
 }
