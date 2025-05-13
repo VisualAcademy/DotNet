93020e6bb8b2a9ec8d2be3eed25ed075d512e41c
Park YongJune <redplus@redplus.net>
2025-05-14 04:32:29 +0900

    .

--- DotNet/DotNet/18_Array/27_ArrayWithVarKeyword/ArrayWithVarKeyword.cs
+++ DotNet/DotNet/18_Array/27_ArrayWithVarKeyword/ArrayWithVarKeyword.cs
@@ -4,19 +4,19 @@ class ArrayWithVarKeyword
 {
     static void Main()
     {
-        // 정수 형식으로 자동적으로 형식이 설정됨
-        var i = 5; // int i = 5; 
+        // var를 사용하면 컴파일 시 형식이 자동으로 결정됨
+        var i = 5; // 컴파일러가 int 형식으로 추론
         Console.WriteLine("i : {0}, 타입 : {1}", i, i.GetType());
 
-        // 문자열 형식으로 형식화됨
+        // 문자열 리터럴이므로 string 형식으로 추론됨
         var s = "Hello";
         Console.WriteLine("s : {0}, 타입 : {1}", s, s.GetType());
 
-        // 배열 형식
+        // 정수형 배열로 추론됨: int[]
         var numbers = new int[] { 1, 2, 3 };
 
-        // 여기서 var item은 numbers 배열의 요소의 형식, 즉 int 형식임
-        foreach (var item in numbers) 
+        // foreach에서 var는 요소의 형식(int)을 추론함
+        foreach (var item in numbers)
         {
             Console.WriteLine("item : {0}, 타입 : {1}", item, item.GetType());
         }
